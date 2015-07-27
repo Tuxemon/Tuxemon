8162405e37506bba8b3f2e0bd5d4ab242120bd01
William Edwards <shadowapex@gmail.com>
2015-07-27 14:42:17 -0700

    Update travis to use pip instead of apt

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,5 +3,6 @@
 set -e
 set -x
 
-sudo apt-get install python-pygame python-imaging python-yapsy
-pip install pytmx
+#sudo apt-get install python-pygame python-imaging python-yapsy
+pip install hg+http://bitbucket.org/pygame/pygame
+pip install pytmx Pillow Yapsy

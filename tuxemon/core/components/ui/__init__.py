f5a531044ad9eed5c720aedb9b24b971a6155fab
Tom Elssjö <tom.elssjo@hotmail.com>
2015-12-13 00:21:53 +0100

    Fixed crash in monster appear

--- tuxemon/core/components/ui/__init__.py
+++ tuxemon/core/components/ui/__init__.py
@@ -59,6 +59,12 @@ class UserInterface(object):
     def __init__(self, images, position, screen, scale=True,
                  animation_speed=0.2, animation_loop=False):
 
+        # Python 2
+        try:
+            unicode
+        except NameError:
+            unicode = str
+
         # Handle loading a single image, multiple images, or surfaces
         if type(images) is str or type(images) is unicode:
             if prepare.BASEDIR not in images:
@@ -113,6 +119,8 @@ class UserInterface(object):
         self.moving = False
         self.move_destination = (0, 0)
         self.move_delta = (0, 0)
+        print("aps")
+        print(self.move_delta)
         self.move_duration = 0.
         self.move_time = 0.
         self.fading = False
@@ -160,8 +168,11 @@ class UserInterface(object):
             else:
                 if type(self.position) is tuple:
                     self.position = list(self.position)
-                self.position[0] -= (mdt[0] * dt) / dur
-                self.position[1] -= (mdt[1] * dt) / dur
+
+                mdt_list = list(mdt)
+                
+                self.position[0] -= (mdt_list[0] * dt) / dur
+                self.position[1] -= (mdt_list[1] * dt) / dur
 
 
     def draw(self):

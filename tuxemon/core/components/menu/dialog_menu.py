b207c9bfec32105419a09c946303f83d76f80227
William Edwards <shadowapex@gmail.com>
2015-12-13 22:04:16 -0800

    Add 'dialog_chain' event to easily create chains of dialog

--- tuxemon/core/components/menu/dialog_menu.py
+++ tuxemon/core/components/menu/dialog_menu.py
@@ -10,10 +10,14 @@ class DialogMenu(Menu):
         Menu.__init__(self, screen, resolution, game, name)
         self.delay = 0.5
         self.elapsed_time = self.delay
+        self.dialog_stack = []
 
     def get_event(self, event):
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            if len(self.dialog_stack) > 0:
+                self.text = self.dialog_stack.pop(0)
+                return
 
             # Hide the menu
             self.visible = False

2784a898a6a462313ce745e8050807c4e8feed0a
buddja <buddjad@gmail.com>
2016-06-28 14:37:38 -0500

    removed bugfix
    Will leave this bug for later

--- tuxemon/core/states/combat/combat.py
+++ tuxemon/core/states/combat/combat.py
@@ -710,9 +710,6 @@ class CombatState(CombatAnimations):
         fadeout_action.parameters = [1000]
         event_engine.actions["fadeout_music"]["method"](self.game, fadeout_action)
 
-        #Round player movement in case battle started in the middle of a step
-        self.game.player1.tile_pos = (round(self.game.player1.tile_pos[0]),round(self.game.player1.tile_pos[1]))
-
         # remove any menus that may be on top of the combat state
         while self.game.current_state is not self:
             self.game.pop_state()

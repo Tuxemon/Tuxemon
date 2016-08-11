73907f117988bb66ad36fe4e63817d8279f5cd4b
bitcraft <leif.theden@gmail.com>
2016-08-11 08:52:55 -0500

    revert linting

--- tuxemon/core/components/menu/__init__.py
+++ tuxemon/core/components/menu/__init__.py
@@ -3,7 +3,7 @@
 #
 # Tuxemon
 # Copyright (C) 2014, William Edwards <shadowapex@gmail.com>,
-# Benjamin Bean <superman2k5@gmail.com>
+#                     Benjamin Bean <superman2k5@gmail.com>
 #
 # This file is part of Tuxemon.
 #
@@ -14,11 +14,11 @@
 #
 # Tuxemon is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Tuxemon. If not, see <http://www.gnu.org/licenses/>.
+# along with Tuxemon.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Contributor(s):
 #

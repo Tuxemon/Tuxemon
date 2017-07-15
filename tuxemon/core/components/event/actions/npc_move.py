8a49cffd4b604e134c56497cb5f67cd4d2426498
bitcraft <leif.theden@gmail.com>
2017-07-14 22:13:03 -0500

    accurate movement speed

---
+++ tuxemon/core/components/event/actions/npc_move.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Tuxemon
+# Copyright (c) 2014-2017 William Edwards <shadowapex@gmail.com>,
+#                         Benjamin Bean <superman2k5@gmail.com>
+#
+# This file is part of Tuxemon
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import absolute_import
+
+from core.components.event import get_npc
+from core.components.event.eventaction import EventAction
+
+
+class NpcMoveAction(EventAction):
+    """ Makes the move to a tile location
+
+    This action blocks until the destination is reached.
+
+    Valid Parameters: npc_slug,
+
+    Direction parameter can be: "left", "right", "up", or "down"
+    """
+    name = "npc_face"
+    valid_parameters = [
+        (str, "npc_slug"),
+        (int, "pos_x"),
+        (int, "pos_y"),
+        (int, "speed")
+    ]
+
+    def start(self):
+        self.npc = get_npc(self.game, self.parameters.npc_slug)
+        self.npc.move_to((self.parameters.pos_x, self.parameters.pos_y), self.parameters.speed)
+
+    def update(self):
+        pass

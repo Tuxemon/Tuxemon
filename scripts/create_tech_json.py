320bfbf6f5beb6f920b86f23b64d68965e92e53a
Keri Volans <keri.volans@gmail.com>
2018-10-22 22:35:03 +0100

    Add scripts

---
+++ scripts/create_tech_json.py
@@ -0,0 +1,91 @@
+"""
+This is used to create the json files for the techniques.
+Put the techniques.xlsx file in the scripts folder alongside this.
+Call this script from the root folder.
+"name_trans.txt" will also be created, which contains the english locale entries, and can
+be copy-pasted into en_US.json
+"""
+
+from collections import namedtuple
+from json import dump
+from openpyxl import load_workbook
+
+excel_path = 'scripts/techniques.xlsx'
+wb2 = load_workbook(excel_path)
+tech_sheet = wb2.get_sheet_by_name('Techs')
+
+DataRow = namedtuple("DataRow", [
+    "name",
+    "id",
+    "element",
+    "recharge",
+    "accuracy",
+    "potency",
+    "user_condition",
+    "target_condition",
+    "animation",
+    "animation_target",
+    "range",
+    "power",
+    "healing_power",
+    "is_fast",
+    "is_area"
+])
+
+
+def create_json(data_row):
+    name = data_row.name.lower().replace(" ", "_").replace("-", "_")
+    name_trans['technique_%s_name' % name] = data_row.name.strip()
+    types = [t.strip().lower() for t in data_row.element.split(",")]
+    effects = []
+    if data_row.power:
+        effects += ["damage"]
+
+    template = {
+        "slug": "technique_%s" % name,
+        "name_trans": "technique_%s_name" % name,
+        "execute_trans": "combat_used_x",
+        "success_trans": None,
+        "failure_trans": "combat_miss",
+        "animation": data_row.animation,
+        "sfx": "blaster1.ogg",
+        "icon": "",
+        "accuracy": data_row.accuracy,
+        "category": "physical",
+        "effects": effects,
+        "healing_power": float(data_row.healing_power) if data_row.healing_power else 0,
+        "is_area": bool(data_row.is_area),
+        "is_fast": bool(data_row.is_fast),
+        "potency": data_row.potency,
+        "power": float(data_row.power) if data_row.power else 0,
+        "range": data_row.range.lower(),
+        "recharge": int(data_row.recharge),
+        "sort": "damage",
+        "target": {
+            "enemy monster": 2,
+            "enemy team": 0,
+            "enemy trainer": 0,
+            "own monster": 1,
+            "own team": 0,
+            "own trainer": 0,
+            "item": 0
+        },
+        "types": types,
+    }
+
+    path = "tuxemon/resources/db/technique/%s.json" % name
+    with open(path, "w") as f:
+        dump(template, f, indent=2, separators=(",", ": "), sort_keys=True)
+
+
+name_trans = {}
+
+for y in range(2, 6000):
+    row = DataRow(*(tech_sheet[y][x].value for x in range(0, 15)))
+    if row.name is None:
+        break
+    create_json(row)
+
+path = "name_trans.txt"
+with open(path, "w") as f:
+    dump(name_trans, f, indent=2, separators=(",", ": "), sort_keys=True)
---
+++ scripts/set_movelist.py
@@ -0,0 +1,152 @@
+"""
+This is used to update the movelists of all the monsters.
+Put the techniques.xlsx file in the scripts folder alongside this.
+The movelists are expected to be in "Sheet4". There should be a row of mon names, each having three moves
+underneath.
+Call this script from the root folder.
+Any missing movelists, or missing json files will be logged to stdout and to a file.
+"""
+
+import glob
+import re
+from json import loads
+from openpyxl import load_workbook
+
+
+PRINT_SUMMARY = True
+excel_path = 'scripts/techniques.xlsx'
+wb2 = load_workbook(excel_path)
+move_list = wb2.get_sheet_by_name('Sheet4')
+
+defaults = {
+    "Water": [
+        "Flood",
+        "Flow",
+        "Font",
+    ],
+    "Wood": [
+        "Fluff Up",
+        "Sting",
+        "Splinter",
+    ],
+    "Earth": [
+        "Mudslide",
+        "Ram",
+        "Rock",
+    ],
+    "Fire": [
+        "Fire Ball",
+        "Fire Claw",
+        "Flamethrower",
+    ],
+    "Metal": [
+        "Perfect Cut",
+        "Shrapnel",
+        "Wall of Steel",
+    ]
+}
+
+missing_json_file = set()
+mons = {}
+name_trans = {}
+updated_mons = set()
+mons_without_a_moveset = set()
+
+
+def set_moves(f, data, moves):
+    moves_string = '''"moveset": [
+        {
+            "level_learned": 2,
+            "technique": "%s"
+        },
+        {
+            "level_learned": 2,
+            "technique": "%s"
+        },
+        {
+            "level_learned": 2,
+            "technique": "%s"
+        }
+    ],''' % (
+        moves[0],
+        moves[1],
+        moves[2],
+    )
+    out = re.sub(r'"moveset": \[(.*?)\],', moves_string, data, flags=re.S)
+    f.truncate(0)
+    f.write(out)
+
+
+def slugify(prefix, data):
+    return "%s_%s" % (prefix, data.lower().replace(" ", "_").replace("-", "_"))
+
+
+for x in range(0, 2000):
+    try:
+        row = [move_list[y][x].value for y in range(1, 5)]
+    except IndexError:
+        break
+    if None in row:
+        continue
+    mons[row[0]] = [
+        slugify("technique", attack)
+        for attack in row[1:]
+    ]
+
+
+for mon, moves in mons.items():
+    path = "tuxemon/resources/db/monster/%s.json" % mon.lower()
+    data = None
+    try:
+        with open(path, "r") as f:
+            data = f.read()
+            updated_mons.add(path)
+    except IOError:
+        missing_json_file.add(mon)
+        continue
+
+    with open(path, "w") as f:
+        set_moves(f, data, moves)
+
+
+if missing_json_file:
+    path = "mons_without_a_json_file.txt"
+    print("%s mons have movelists, but don't have a json file. See %s for a summary" % (
+        len(missing_json_file),
+        path,
+    ))
+    print(missing_json_file)
+    if PRINT_SUMMARY:
+        with open(path, "w") as f:
+            for mon in missing_json_file:
+                f.write(mon + "\n")
+
+monster_path = "tuxemon/resources/db/monster/*.json"
+jsons = glob.glob(monster_path)
+for path in jsons:
+    if path not in updated_mons:
+        with open(path, "r") as f2:
+            data = f2.read()
+            t = loads(data)['types'][0]
+            moves = [slugify("technique", m) for m in defaults[t]]
+
+        with open(path, "w") as f2:
+            set_moves(f2, data, moves)
+
+        mon = path.rpartition("/")[2].partition(".")[0]
+        mons_without_a_moveset.add(mon)
+
+if mons_without_a_moveset:
+    path = "mons_without_a_moveset.txt"
+    print(
+            "%s existing mons didn't have a movelist defined in the excel doc. "
+            "Used default moveset for type. See %s for a summary" % (
+                len(mons_without_a_moveset),
+                path,
+            )
+    )
+    print(mons_without_a_moveset)
+    if PRINT_SUMMARY:
+        with open(path, "w") as f:
+            for mon in missing_json_file:
+                f.write(mon + "\n")

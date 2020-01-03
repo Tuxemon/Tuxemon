73a751f908ccdceaee72710707f9dacb0e571d99
Leif Theden <leif.theden@gmail.com>
2020-01-03 16:33:29 -0600

    Update build_win32.py

--- build_win32.py
+++ build_win32.py
@@ -26,7 +26,7 @@ build_exe_options = {'packages': packages,
 if __name__ == '__main__':
     setup(
         name='Tuxemon',
-        version='0.4.1',
+        version='0.4.2',
         options={'build_exe': build_exe_options},
         description='Open source RPG',
         executables=[Executable('tuxemon.py', icon='tuxemon/resources/gfx/icon.ico')],

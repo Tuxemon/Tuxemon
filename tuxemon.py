a563d2a61fa8cb84a5b23f54ab9eb46479283f77
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2020-01-07 18:44:00 +0200

    Allow specifying the starting map

--- tuxemon.py
+++ tuxemon.py
@@ -44,9 +44,11 @@ import tuxemon.core.main
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-l', dest='slot', metavar='1,2,3', type=int, nargs='?',
+    parser.add_argument('-l', '--load', dest='slot', metavar='1,2,3', type=int, nargs='?',
                         default=None, help='The index of the save file to load')
-    parser.add_argument('-d', dest='data', metavar='resources', type=str, nargs='?',
+    parser.add_argument('-d', '--data', dest='data', metavar='resources', type=str, nargs='?',
                         default=None, help='The data directory to use')
+    parser.add_argument('-s', '--starting-map', dest='starting_map', metavar='bedroom.tmx', type=str, nargs='?',
+                        default=None, help='The starting map')
     args = parser.parse_args()
     tuxemon.core.main.main(args)

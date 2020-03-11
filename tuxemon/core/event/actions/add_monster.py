d993aa67cbe1130f56a6ac88cf29b17d730b55ac
Adam Chevalier <chevalierAdam2@gmail.com>
2020-03-11 08:12:54 -0500

    Fix #688 and some other issues (#692)
    * add pairagrin to the game
    
    no art files yet
    
    * merging upstream
    
    * small changes mostly for testing
    
    * cleanup accidental change
    
    * final adjustments to ensure the value is read in correctly
    
    * fix small regression
    
    * fix several issues with items
    
    * remove unnecessary logging import
    
    * responding to code review

--- tuxemon/core/event/actions/add_monster.py
+++ tuxemon/core/event/actions/add_monster.py
@@ -25,7 +25,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from tuxemon.core import monster
-from tuxemon.core.locale import T
 from tuxemon.core.event.eventaction import EventAction
 
 

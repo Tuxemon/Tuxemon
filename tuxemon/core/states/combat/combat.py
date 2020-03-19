Tuxemon/Tuxemon

commit 27478521eb138a3b8931279143ea30d65b29bb54

Split audio/graphics functions from tools.py into own files (#690)
* Split audio/graphics functions from tools.py into own files

* Fix regression with functools due to search/replace

* Remove logger import from audio.py

* Audio.py formatting

* Simplify imports in play_sound.py

* Simplify imports in graphics.py, formatting

* Remove debugging statements in item.py

* Optimise imports in menu.py

* Optimise imports in combat.py

* Optimise imports in combat_animations.py

* Remove debuging code in items

* Optimise imports in splash screen

* Replace search/replace error

* Fix import in combat_animations

* Revent docstring

* Fix circular import in monster.py and graphics.py

* Fix imports

* Fix imports

tuxemon/core/states/combat/combat.py
@@ -40,15 +40,15 @@ from itertools import chain
 
 import pygame
 
-from tuxemon.core import state, tools
+from tuxemon.core import audio, state, tools, graphics
 from tuxemon.core.combat import check_status, fainted, get_awake_monsters, defeated
 from tuxemon.core.locale import T
+from tuxemon.core.platform.const import buttons
 from tuxemon.core.pyganim import PygAnimation
 from tuxemon.core.sprite import Sprite
 from tuxemon.core.technique import Technique
 from tuxemon.core.ui.draw import GraphicBox
 from tuxemon.core.ui.text import TextArea
-from tuxemon.core.platform.const import buttons
 from .combat_animations import CombatAnimations
 
 logger = logging.getLogger(__name__)
@@ -425,11 +425,11 @@ class CombatState(CombatAnimations):
         def add(menuitem):
             monster = menuitem.game_object
             if monster.current_hp == 0:
-                tools.open_dialog(self.game, [T.format("combat_fainted", parameters={"name": monster.name})])
+                graphics.open_dialog(self.game, [T.format("combat_fainted", parameters={"name": monster.name})])
             elif monster in self.active_monsters:
-                tools.open_dialog(self.game, [T.format("combat_isactive", parameters={"name": monster.name})])
+                graphics.open_dialog(self.game, [T.format("combat_isactive", parameters={"name": monster.name})])
                 msg = T.translate("combat_replacement_is_fainted")
-                tools.open_dialog(self.game, [msg])
+                graphics.open_dialog(self.game, [msg])
             else:
                 self.add_monster_into_play(player, monster)
                 self.game.pop_state()
@@ -515,7 +515,7 @@ class CombatState(CombatAnimations):
         x, y, w, h = self.game.screen.get_rect()
         rect = pygame.Rect(0, 0, w, h // 4)
         rect.bottomright = w, h
-        border = tools.load_and_scale(self.borders_filename)
+        border = graphics.load_and_scale(self.borders_filename)
         self.dialog_box = GraphicBox(border, None, self.background_color)
         self.dialog_box.rect = rect
         self.sprites.add(self.dialog_box, layer=100)
@@ -643,7 +643,7 @@ class CombatState(CombatAnimations):
             message = ''
 
         try:
-            tools.load_sound(technique.sfx).play()
+            audio.load_sound(technique.sfx).play()
         except AttributeError:
             pass
 
@@ -803,7 +803,7 @@ class CombatState(CombatAnimations):
         frame_time = .09
         images = list()
         for fn in technique.images:
-            image = tools.load_and_scale(fn)
+            image = graphics.load_and_scale(fn)
             images.append((image, frame_time))
 
         tech = PygAnimation(images, False)

tuxemon/core/audio.py
@@ -1,0 +1,49 @@
+import logging
+import os.path
+
+import pygame
+from pygame import mixer
+
+from tuxemon.core.db import db
+from tuxemon.core.tools import transform_resource_filename
+
+logger = logging.getLogger(__name__)
+
+
+def load_sound(slug):
+    """ Load a sound from disk, identified by it's slug in the db
+
+    :param slug: slug for the file record to load
+    :type slug: String
+    :rtype: core.platform.mixer.Sound
+    """
+
+    class DummySound(object):
+        def play(self):
+            pass
+
+    # get the filename from the db
+    filename = db.lookup_file("sounds", slug)
+    filename = transform_resource_filename("sounds", filename)
+
+    # on some platforms, pygame will silently fail loading
+    # a sound if the filename is incorrect so we check here
+    if not os.path.exists(filename):
+        msg = 'audio file does not exist: {}'.format(filename)
+        logger.error(msg)
+        return DummySound()
+
+    try:
+        return mixer.Sound(filename)
+    except MemoryError:
+        # raised on some systems if there is no mixer
+        logger.error('memoryerror, unable to load sound')
+        return DummySound()
+    except pygame.error as e:
+        # pick one:
+        # * there is no mixer
+        # * sound has invalid path
+        # * mixer has no output (device ok, no speakers)
+        logger.error(e)
+        logger.error('unable to load sound')
+        return DummySound()

tuxemon/core/event/actions/dialog.py
@@ -28,7 +28,8 @@ import logging
 
 from tuxemon.core.locale import replace_text
 from tuxemon.core.event.eventaction import EventAction
-from tuxemon.core.tools import open_dialog, get_avatar
+from tuxemon.core.tools import open_dialog
+from tuxemon.core.graphics import get_avatar
 
 logger = logging.getLogger(__name__)
 

tuxemon/core/event/actions/dialog_chain.py
@@ -28,7 +28,8 @@ import logging
 
 from tuxemon.core.locale import replace_text
 from tuxemon.core.event.eventaction import EventAction
-from tuxemon.core.tools import open_dialog, get_avatar
+from tuxemon.core.tools import open_dialog
+from tuxemon.core.graphics import get_avatar
 
 logger = logging.getLogger(__name__)
 

tuxemon/core/event/actions/play_map_animation.py
@@ -28,7 +28,7 @@ import logging
 
 from tuxemon.core import prepare
 from tuxemon.core.event.eventaction import EventAction
-from tuxemon.core.tools import load_animation_from_frames
+from tuxemon.core.graphics import load_animation_from_frames
 
 logger = logging.getLogger(__name__)
 

tuxemon/core/event/actions/play_sound.py
@@ -24,8 +24,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from tuxemon.core import tools
-from tuxemon.core.db import db
+from tuxemon.core import audio
 from tuxemon.core.event.eventaction import EventAction
 
 
@@ -41,5 +40,5 @@ class PlaySoundAction(EventAction):
 
     def start(self):
         filename = self.parameters.filename
-        sound = tools.load_sound(filename)
+        sound = audio.load_sound(filename)
         sound.play()

tuxemon/core/event/actions/translated_dialog.py
@@ -28,7 +28,8 @@ import logging
 
 from tuxemon.core.locale import process_translate_text
 from tuxemon.core.event.eventaction import EventAction
-from tuxemon.core.tools import open_dialog, get_avatar
+from tuxemon.core.tools import open_dialog
+from tuxemon.core.graphics import get_avatar
 
 logger = logging.getLogger(__name__)
 

tuxemon/core/event/actions/translated_dialog_chain.py
@@ -28,7 +28,8 @@ import logging
 
 from tuxemon.core.locale import process_translate_text
 from tuxemon.core.event.eventaction import EventAction
-from tuxemon.core.tools import open_dialog, get_avatar
+from tuxemon.core.tools import open_dialog
+from tuxemon.core.graphics import get_avatar
 
 logger = logging.getLogger(__name__)
 

commit a81321692d2a3d7b710e7968142e8241b4b1d721

Fix bad function call caused by #690

tuxemon/core/states/combat/combat.py
@@ -425,11 +425,11 @@ class CombatState(CombatAnimations):
         def add(menuitem):
             monster = menuitem.game_object
             if monster.current_hp == 0:
-                graphics.open_dialog(self.game, [T.format("combat_fainted", parameters={"name": monster.name})])
+                tools.open_dialog(self.game, [T.format("combat_fainted", parameters={"name": monster.name})])
             elif monster in self.active_monsters:
-                graphics.open_dialog(self.game, [T.format("combat_isactive", parameters={"name": monster.name})])
+                tools.open_dialog(self.game, [T.format("combat_isactive", parameters={"name": monster.name})])
                 msg = T.translate("combat_replacement_is_fainted")
-                graphics.open_dialog(self.game, [msg])
+                tools.open_dialog(self.game, [msg])
             else:
                 self.add_monster_into_play(player, monster)
                 self.game.pop_state()
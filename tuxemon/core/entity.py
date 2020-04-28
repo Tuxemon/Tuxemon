9ea8d55077df9289c621c0f9a348bb8f2ac4a9cd
Leif Theden <leif.theden@gmail.com>
2020-04-28 10:50:21 -0500

    wip

--- tuxemon/core/entity.py
+++ tuxemon/core/entity.py
@@ -28,7 +28,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from tuxemon.core.world import WorldBody
+from tuxemon.core.euclid import Point2, Vector3, Point3
+from tuxemon.core.map import proj
 
 
 class Entity(object):
@@ -44,7 +45,57 @@ class Entity(object):
         self.slug = None
         self.world = None
         self.instance_id = None
-        self.body = WorldBody()
+        self.tile_pos = Point2(0, 0)
+        self.position3 = Point3(0, 0, 0)
+        self.acceleration3 = Vector3(0, 0, 0)  # not used currently, just set velocity
+        self.velocity3 = Vector3(0, 0, 0)
+        self.update_location = False
+
+    # === PHYSICS START ================================================================
+    def stop_moving(self):
+        """ Completely stop all movement
+
+        :return: None
+        """
+        self.velocity3.x = 0
+        self.velocity3.y = 0
+        self.velocity3.z = 0
+
+    def pos_update(self):
+        """ WIP.  Required to be called after position changes
+
+        :return:
+        """
+        self.tile_pos = proj(self.position3)
+
+    def update_physics(self, td):
+        """ Move the entity according to the movement vector
+
+        :param td:
+        :return:
+        """
+        self.position3 += self.velocity3 * td
+        self.pos_update()
+
+    def set_position(self, pos):
+        """ Set the entity's position in the game world
+
+        :param pos:
+        :return:
+        """
+        self.position3.x = pos[0]
+        self.position3.y = pos[1]
+        self.pos_update()
+
+    # === PHYSICS END ==================================================================
+
+    @property
+    def moving(self):
+        """ Is the entity moving?
+
+        :rtype: bool
+        """
+        return not self.velocity3 == (0, 0, 0)
 
     def get_state(self, session):
         """ Get Entities internal state for saving/loading

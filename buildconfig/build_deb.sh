52689529f3aa9fe8be809698972d086fe81364d9
Leif Theden <leif.theden@gmail.com>
2020-09-10 11:28:34 -0500

    travis: update deb build scripts for tests, name

--- buildconfig/build_deb.sh
+++ buildconfig/build_deb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+export PYBUILD_DISABLE=test
 cd tuxemon-*
 cp ../dist/* ..
 debmake -b':py3'
 dpkg-buildpackage -us -uc
-cd ../..
-mv ./dist/tuxemon*.deb ./build/tuxemon-unstable-latest.deb
+cd ..
+mv ./dist/tuxemon*.deb ./build/tuxemon-$TRAVIS_BRANCH.deb

048f87cfeea1bece5ca7445b080af2ce22dfe1f6
Leif Theden <leif.theden@gmail.com>
2020-09-10 14:46:14 -0500

    travis: remove unused opk build stuff

--- buildconfig/build_opk.sh
+++ buildconfig/build_opk.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sed -i 's/resolution_x.*/resolution_x = 320/g' tuxemon/tuxemon.cfg
-sed -i 's/resolution_y.*/resolution_y = 240/g' tuxemon/tuxemon.cfg
-sed -i 's/fullscreen.*/fullscreen = 1/g' tuxemon/tuxemon.cfg
-cp tuxemon/resources/gfx/icon_32.png tuxemon/resources/gfx/icon.png
 mksquashfs tuxemon ./build/tuxemon-unstable-latest.opk -all-root -noappend -no-exports -no-xattrs

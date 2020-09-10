3cffd870f8635cec0119f8bee441cc12d67ecc53
Leif Theden <leif.theden@gmail.com>
2020-09-10 14:16:14 -0500

    travis: diagnose win32 build

--- buildconfig/build_win32.sh
+++ buildconfig/build_win32.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-# embedded WIP
-#cd pywin64
-#wget https://www.python.org/ftp/python/3.8.5/python-3.8.5-embed-amd64.zip
-#wget https://bootstrap.pypa.io/get-pip.py
-#unzip *zip
-#echo "import site" >> python38._pth
-#wine python get-pip.py
-#cd ..
-#wine pywin64/Scripts/pip install -r requirements.txt
-#wine pywin64/Scripts/pip install cx_freeze
-#wine pywin64/python.exe buildconfig/setup_win32.py build
-
-# install with full python installer
 wget https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe
 xvfb-run wine64 python-3.8.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
 wine64 pip install -r requirements.txt
--- buildconfig/prebuild_win32.sh
+++ buildconfig/prebuild_win32.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install wget wine64 xvfb

704f5a6c8bd1e4ae1728b0298af77cc6638f3a66
Leif Theden <leif.theden@gmail.com>
2020-09-10 09:31:36 -0500

    travis: python3 and apt-get update

--- buildconfig/prebuild_deb.sh
+++ buildconfig/prebuild_deb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt install -y fakeroot git curl squashfs-tools sed debmake debhelper python3-pygame python3-all python3-setuptools
+sudo apt-get update
+sudo apt-get install -y fakeroot debmake debhelper python3-pygame python3-all python3-setuptools
---
+++ buildconfig/prebuild_opk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install sed squashfs-tools curl

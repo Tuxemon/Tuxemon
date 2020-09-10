2b2ccdf456718f55b7ac3a6c59bfba71324446c8
Leif Theden <leif.theden@gmail.com>
2020-09-10 15:41:31 -0500

    travis: fiddle with wine64/32

--- buildconfig/build_win32.sh
+++ buildconfig/build_win32.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe
 xvfb-run wine64 python-3.8.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
-wine64 pip install -r requirements.txt
-wine64 pip install cx_freeze
-wine64 python buildconfig/setup_win32.py build
+wine pip install -r requirements.txt
+wine pip install cx_freeze
+wine python buildconfig/setup_win32.py build

f3f7949807b7fd8ae94064306a8ab7a43309d64e
Leif Theden <leif.theden@gmail.com>
2020-09-10 15:34:39 -0500

    travis: add resources to debian builds

--- buildconfig/build_deb.sh
+++ buildconfig/build_deb.sh
@@ -3,6 +3,7 @@ export PYBUILD_DISABLE=test
 cd tuxemon-*
 cp ../dist/* ..
 debmake -b':py3'
+echo "./mods usr/share/tuxemon/"
 dpkg-buildpackage -us -uc
 cd ..
 mv tuxemon*.deb build/tuxemon-$TRAVIS_BRANCH.deb

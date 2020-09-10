9211cc9430e7e79b84afd15249cf5a9b25d98273
Leif Theden <leif.theden@gmail.com>
2020-09-10 15:40:06 -0500

    travis: quiet win32 python download

--- buildconfig/build_win32.sh
+++ buildconfig/build_win32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe
+wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe
 xvfb-run wine64 python-3.8.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
 wine64 pip install -r requirements.txt
 wine64 pip install cx_freeze

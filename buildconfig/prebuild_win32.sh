267f2c5893e1a9ecaffee82d7d19d77e2c7cec83
Leif Theden <leif.theden@gmail.com>
2020-09-10 23:38:27 -0500

    travis: magic to get the wine deps installed

--- buildconfig/prebuild_win32.sh
+++ buildconfig/prebuild_win32.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install wget wine32 wine64 xvfb
+sudo apt-get install wget xvfb
+sudo apt-get install -o APT::Immediate-Configure=false wine32 wine64
+sudo apt-get -f install
+sudo dpkg --configure -a
+sudo apt-get dist-upgrade

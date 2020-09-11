f910bfaabc5a24cfeeaadf715ce0d9dc9c84b032
Leif Theden <leif.theden@gmail.com>
2020-09-10 23:48:31 -0500

    travis: tweak wine32 build

--- buildconfig/prebuild_win32.sh
+++ buildconfig/prebuild_win32.sh
@@ -2,7 +2,7 @@
 sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install wget xvfb
-sudo apt-get install -o APT::Immediate-Configure=false wine32 wine64
-sudo apt-get -f install
-sudo dpkg --configure -a
-sudo apt-get dist-upgrade
+sudo apt-get install -o APT::Immediate-Configure=false wine wine32 wine64
+# sudo apt-get -f install
+# sudo dpkg --configure -a
+# sudo apt-get dist-upgrade

4d8f5e501bcc4da960ca42c9f3f0d6e759b83ddc
Leif Theden <leif.theden@gmail.com>
2020-09-12 09:09:25 -0500

    travis: use wheels on wine builds

--- buildconfig/build_wine.sh
+++ buildconfig/build_wine.sh
@@ -7,6 +7,7 @@ rm -rf ~/.wine
 xvfb-run wine wineboot
 wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5.exe
 xvfb-run wine python-3.8.5.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
+wine pip install -U setuptools wheel
 wine pip install -r requirements.txt
 wine pip install cx_freeze
 wine python buildconfig/setup_windows.py build

e4a72510faff61f62dcaac81c8576eeb4a22bb2f
Leif Theden <leif.theden@gmail.com>
2020-09-12 11:00:20 -0500

    travis: revert cx_freeze version

--- buildconfig/build_wine.sh
+++ buildconfig/build_wine.sh
@@ -9,5 +9,5 @@ wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5.exe
 xvfb-run wine python-3.8.5.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
 wine pip install -U setuptools wheel
 wine pip install -r requirements.txt
-wine pip install cx_freeze
+wine pip install cx_freeze==5.1.1
 wine python buildconfig/setup_windows.py build

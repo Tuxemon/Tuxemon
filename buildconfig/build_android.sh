3a4b5229d46e2edbb53128d0341b7ca44975eb5e
Leif Theden <leif.theden@gmail.com>
2020-09-12 17:08:29 -0500

    travis: test with p4a

--- buildconfig/build_android.sh
+++ buildconfig/build_android.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pip install requests
-python buildconfig/build_android.py
+pip install git+https://github.com/pygame/python-for-android.git
+p4a --version
+# python buildconfig/build_android.py
+p4a apk
+  --name Tuxemon \
+  --private tusemon \
+  --version 0.0 \
+  --package=org.tuxemon.Tuxemon \
+  --requirements=python3,pygame

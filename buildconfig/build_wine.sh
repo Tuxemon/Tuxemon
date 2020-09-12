edfae272e8045fba814f74d00cd5af9504729bd8
Leif Theden <leif.theden@gmail.com>
2020-09-12 08:57:31 -0500

    travis: fix reference to renamed build file

--- buildconfig/build_wine.sh
+++ buildconfig/build_wine.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 export WINEARCH=win32
-wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe
-python-3.8.5-amd64.exe /quiet PrependPath=1 Include_doc=0 Include_test=0 SimpleInstall=1
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install wget xvfb wine wine32 wine64 winetricks
+rm -rf ~/.wine
+xvfb-run wine wineboot
+wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5.exe
+xvfb-run wine python-3.8.5.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
 wine pip install -r requirements.txt
 wine pip install cx_freeze
-wine python buildconfig/setup_win32.py build
+wine python buildconfig/setup_windows.py build
--- buildconfig/install_wine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export WINEARCH=win32
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install wget xvfb wine wine32 wine64 winetricks
-rm -rf ~/.wine
-xvfb-run wine wineboot
-wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5.exe
-xvfb-run wine python-3.8.5.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1

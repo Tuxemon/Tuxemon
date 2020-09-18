072cad97d3d297f5c14341e30264144b2070b40d
Leif Theden <leif.theden@gmail.com>
2020-09-17 21:05:37 -0500

    travis: android baby steps

--- buildconfig/build_android.sh
+++ buildconfig/build_android.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-pip install git+https://github.com/pygame/python-for-android.git
+sudo apt-get install build-essential pkg-config python3-dev \
+  autoconf automake libtool libffi-dev cmake python3-pip
+python3 -m pip install setuptools pygame==2.0.0.dev10 Cython
+python3 -m pip install git+https://github.com/pygame/python-for-android.git
 p4a --version
-# python buildconfig/build_android.py
 p4a apk
   --name Tuxemon \
-  --private tusemon \
+  --private tuxemon \
   --version 0.0 \
   --package=org.tuxemon.Tuxemon \
-  --requirements=python3,pygame
+  --requirements=python3,pygame2,libffi \
+  --bootstrap=sdl2
--- buildconfig/install_android.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install -y build-essential ccache git zlib1g-dev python3 python3-dev \
-libncurses5:i386 libstdc++6:i386 zlib1g:i386 openjdk-8-jdk unzip ant ccache \
-autoconf libtool libssl-dev

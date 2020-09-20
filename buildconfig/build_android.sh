c1d7843b0770faaaf90323385016763f5162b0a3
Leif Theden <leif.theden@gmail.com>
2020-09-20 10:28:25 -0500

    travis: various tweaks

--- buildconfig/build_android.sh
+++ buildconfig/build_android.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-echo y | sdkmanager "ndk-bundle"
-export ANDROIDNDK="$ANDROID_HOME/ndk-bundle"
+export ANDROID_BUILD_TOOLS_VERSION=28.0.2
+export ANDROIDAPI=27
+export ANDROID_TOOLS=commandlinetools-linux-6609375_latest.zip
+export ANDROID_HOME=$HOME/android_sdk
+export ANDROID_SDK_ROOT=$HOME/android_sdk
+export ANDROIDNDK=$HOME/android_sdk/ndk-bundle
 sudo apt update
-sudo apt-get install build-essential pkg-config python3.7 python3.7-dev \
-  python3.7-gdbm autoconf automake libtool libffi-dev cmake openssl libssl-dev
-sudo ln -fs /usr/bin/python3.7 /usr/bin/python3
-sudo ln -fs /usr/bin/python3.7 /usr/bin/python
-# do not use a virtualenv
+sudo apt-get -y remove --purge man-db
+sudo apt-get -y install build-essential pkg-config python3.7-dev python3-distutils \
+  python3.7-venv autoconf automake libtool libffi-dev cmake openjdk-8-jdk unzip git \
+  libssl-devel zip
+sudo ln -sf /usr/bin/python3.7 /usr/bin/python3
+wget https://dl.google.com/android/repository/$ANDROID_TOOLS
+mkdir -p $ANDROID_HOME
+unzip -q $ANDROID_TOOLS -d $ANDROID_HOME/cmdline-tools
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/tools/bin
+mkdir -p $HOME/.android
+touch $HOME/.android/repositories.cfg
+sdkmanager --update
+yes | sdkmanager "platform-tools" >/dev/null
+yes | sdkmanager "platforms;android-$ANDROIDAPI" >/dev/null
+yes | sdkmanager "build-tools;$ANDROID_BUILD_TOOLS_VERSION" >/dev/null
+yes | sdkmanager "ndk-bundle"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
-sudo python -m pip install setuptools pygame==2.0.0.dev10 Cython
-sudo python -m pip install git+https://github.com/kivy/python-for-android.git
+sudo python3 get-pip.py
+python3 -m pip install setuptools wheel pygame==2.0.0.dev10 Cython
+python3 -m pip install git+https://github.com/pygame/python-for-android.git
+git clone https://github.com/Tuxemon/Tuxemon.git
+cd Tuxemon
 p4a --version
 p4a apk --name Tuxemon \
   --private tuxemon \
   --version 0.0 \
   --package=org.tuxemon.Tuxemon \
-  --requirements=libffi,python3==3.7.1,pygame2 \
+  --requirements=openssl,libffi,python3==3.7.1,pygame==2.0.0.dev10 \
   --bootstrap=sdl2

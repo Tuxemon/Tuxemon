664810ba06b397c96e6dc8f7d90c3e41f0c5e3c6
Leif Theden <leif.theden@gmail.com>
2020-10-27 08:15:28 -0500

    various fixes for windows

--- buildconfig/build_wine_cx_freeze.sh
+++ buildconfig/build_wine_cx_freeze.sh
@@ -7,7 +7,7 @@ rm -rf ~/.wine
 xvfb-run wine wineboot
 wget -q https://www.python.org/ftp/python/3.8.5/python-3.8.5.exe
 xvfb-run wine python-3.8.5.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
-wine pip install -U setuptools wheel
-wine pip install -r requirements.txt
-wine pip install cx_freeze
+wine python -m pip install -U setuptools wheel cx_Freeze
+wine python -m pip install -r -U requirements.txt
 wine python buildconfig/setup_windows.py build
+cp -a mods build/exe.win32-3.8/

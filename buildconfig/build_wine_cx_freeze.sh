602a3356afcd5741be77e28a0f7c21d5d6e295a9
Leif Theden <leif.theden@gmail.com>
2021-03-01 16:23:48 -0600

    more windows build changes

--- buildconfig/build_wine_cx_freeze.sh
+++ buildconfig/build_wine_cx_freeze.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # debian 10
-./setup_wine_debian10.sh
+buildconfig/setup_wine_debian10.sh
 wine python -m pip install -U setuptools wheel cx_Freeze
 wine python -m pip install -U -r requirements.txt
 find . -name "*pyc" -delete
-wine python buildconfig/setup_windows.py build
-cd build/exe.wine-amd*
-cp .wine/drive_c/Program Files/Python39/python39.dll .
+wine python buildconfig/setup_cx_freeze.py build
+cd build/exe.win*
+cp ~/.wine/drive_c/Program\ Files/Python39/python39.dll .
 cp ../../LICENSE .
 cp ../../CONTRIBUTING.md .
 cp ../../CREDITS.md .
 cp ../../README.md .
 cp ../../SPYDER_README.md .
+cd ../../
+mkdir -p dist/windows_cx_freeze
+cp -a build/exe.win*/* dist/windows_cx_freeze
--- buildconfig/build_wine_pyinstaller.sh
+++ buildconfig/build_wine_pyinstaller.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # debian 10
-./setup_wine_debian10.sh
+buildconfig/setup_wine_debian10.sh
 wine python -m pip install -U setuptools wheel pyinstaller
 wine python -m pip install -U -r requirements.txt
 find . -name "*pyc" -delete
 wine pyinstaller buildconfig/pyinstaller/tuxemon.spec
-cd dist/tuxemon
+cd build/tuxemon
+cp ~/.wine/drive_c/Program\ Files/Python39/python39.dll .
 cp ../../LICENSE .
 cp ../../CONTRIBUTING.md .
 cp ../../CREDITS.md .

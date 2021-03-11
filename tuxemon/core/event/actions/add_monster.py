69d3641f73120f133e8496a4b9c01ecc488bc95e
xirsoi <chevalierAdam2@gmail.com>
2021-03-11 13:09:05 -0600

    adds missing parameter to get_npc call

--- tuxemon/core/event/actions/add_monster.py
+++ tuxemon/core/event/actions/add_monster.py
@@ -23,6 +23,7 @@ from tuxemon.core import monster
 from tuxemon.core.event.eventaction import EventAction
 from tuxemon.core.event import get_npc
 
+
 class AddMonsterAction(EventAction):
     """Adds a monster to the current player's party if there is room.
 
@@ -44,7 +45,7 @@ class AddMonsterAction(EventAction):
         if trainer_slug is None:
             trainer = self.session.player
         else:
-            trainer = get_npc(trainer_slug)
+            trainer = get_npc(self.session, trainer_slug)
 
         current_monster = monster.Monster()
         current_monster.load_from_db(monster_slug)

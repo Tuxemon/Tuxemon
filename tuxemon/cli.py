bdbddfe6900353b45ec1dfc0e77c377d6e5493f1
vnmabus <vnmabus@gmail.com>
2021-06-30 16:33:04 +0200

    Typing cli module.

--- tuxemon/cli.py
+++ tuxemon/cli.py
@@ -26,39 +26,48 @@
 # cli Command line module used for debugging.
 #
 #
-
+from __future__ import annotations
 import cmd
 import code
 import logging
 import pprint
 from threading import Thread
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tuxemon.client import Client
 
 logger = logging.getLogger(__name__)
 
 
 class CommandLine(cmd.Cmd):
-    """A class to enable an interactive debug command line. Provides a full python shell to review
+    """
+    A class to enable an interactive debug command line.
+
+    Provides a full python shell to review
     and modify variables while the game is actively running.
 
-    :param app: The tuxemon.Game object of the game itself.
+    Parameters:
+        app: The game object itself.
 
-    :type app: tuxemon.Game
-
-    To include the command line in the game, simply add the following line under the
-    initialization of the main game:
+    To include the command line in the game, simply add the following line
+    under the initialization of the main game:
 
     >>> def __init__(self):
     ...     self.cli = cli.CommandLine(self)
 
     """
 
-    def __init__(self, app):
+    def __init__(self, app: Client) -> None:
         # Initiate the parent class
         cmd.Cmd.__init__(self)
 
         # Set up the command line prompt itself
         self.prompt = "Tuxemon>> "
-        self.intro = 'Tuxemon CLI\nType "help", "copyright", "credits"' ' or "license" for more information.'
+        self.intro = (
+            'Tuxemon CLI\nType "help", "copyright", "credits"'
+            ' or "license" for more information.'
+        )
 
         # set up a  pretty printer so that shit is formatted nicely
         self.pp = pprint.PrettyPrinter(indent=4)
@@ -70,85 +79,85 @@ class CommandLine(cmd.Cmd):
         self.cmd_thread.daemon = True
         self.cmd_thread.start()
 
-    def emptyline(self):
-        """If an empty line was entered at the command line, do nothing.
+    def emptyline(self) -> bool:
+        """If an empty line was entered at the command line, do nothing."""
+        return False
 
-        :param None:
-
-        :rtype: None
-        :returns: None
-
+    def do_exit(self, line: str) -> bool:
         """
+        Exit the program on "exit".
 
-    def do_exit(self, line):
-        """If "exit" was typed on the command line, set the app's exit variable to True.
+        If "exit" was typed on the command line, set the app's exit variable
+        to True.
 
-        :param None:
-
-        :rtype: None
-        :returns: None
+        Parameters:
+            line: Ignored.
 
         """
         self.app.exit = True
         return True
 
-    def do_quit(self, line):
-        """If "quit" was typed on the command line, set the app's exit variable to True.
+    def do_quit(self, line: str) -> bool:
+        """
+        Exit the program on "quit".
 
-        :param None:
+        If "quit" was typed on the command line, set the app's exit variable
+        to True.
 
-        :rtype: None
-        :returns: None
+        Parameters:
+            line: Ignored.
 
         """
         self.app.exit = True
         return True
 
-    def do_EOF(self, line):
-        """If you press CTRL-D on the command line, set the app's exit variable to True.
+    def do_EOF(self, line: str) -> bool:
+        """
+        Exit the program on EOF.
 
-        :param None:
+        If you press CTRL-D on the command line, set the app's exit variable
+        to True.
 
-        :rtype: None
-        :returns: None
+        Parameters:
+            line: Ignored.
 
         """
-        self.do_quit(line)
-        return True
+        return self.do_quit(line)
 
-    def do_copyright(self, line):
-        """Print the copyright information if "copyright" was entered.
+    def do_copyright(self, line: str) -> None:
+        """
+        Print the copyright information if "copyright" was entered.
 
-        :param None:
-
-        :rtype: None
-        :returns: None
+        Parameters:
+            line: Ignored.
 
         """
         print(
-            "Tuxemon\nCopyright (C) 2014, William Edwards <shadowapex@gmail.com>, Benjamin Bean <superman2k5@gmail.com>"
+            "Tuxemon\nCopyright (C) 2014,"
+            " William Edwards <shadowapex@gmail.com>,"
+            " Benjamin Bean <superman2k5@gmail.com>"
         )
 
-    def do_credits(self, line):
-        """Print the copyright information if "credits" was entered.
+    def do_credits(self, line: str) -> None:
+        """
+        Print the copyright information if "credits" was entered.
 
-        :param None:
-
-        :rtype: None
-        :returns: None
+        Parameters:
+            line: Ignored.
 
         """
         self.do_copyright(line)
 
-    def do_python(self, line):
-        """Open a full python shell if "python" was typed in the command line. From here, you can
-        look at and manipulate any variables in the application. This can be used to look at this
+    def do_python(self, line: str) -> None:
+        """
+        Open a full python shell if "python" was typed in the command line.
+
+        From here, you can look at and manipulate any variables in the
+        application. This can be used to look at this
         instance's "self.app" variable which contains the game object.
 
-        :param None:
-
-        :rtype: None
-        :returns: None
+        Parameters:
+            line: Ignored.
 
         """
         print("Available variables:")
@@ -156,15 +165,13 @@ class CommandLine(cmd.Cmd):
         self.pp.pprint(self.__dict__)
         code.interact(local=locals())
 
-    def postcmd(self, stop, line):
-        """If the application has exited, exit here as well.
+    def postcmd(self, stop: bool, line: str) -> bool:
+        """
+        If the application has exited, exit here as well.
 
-        :param None:
-
-        :rtype: None
-        :returns: None
+        Parameters:
+            line: Ignored.
 
         """
         # Check to see if we have exited
-        if self.app.exit:
-            return True
+        return self.app.exit

80e5647616828c3bade13bec60dc3fe8c1b364ca
Sanglorian <sanglorian@gmail.com>
2021-07-04 22:00:19 +1000

    Consolidated core_tiles
    I have created core_buildings, core_set pieces and core_outdoor, with accompanying TSX files.

---
+++ mods/tuxemon/gfx/tilesets/core_set pieces..tsx
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="core_set pieces" tilewidth="16" tileheight="16" tilecount="744" columns="31">
+ <image source="core_set pieces.png" width="496" height="384"/>
+ <tile id="143">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="146">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="149">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="152">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="175">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="176">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="312">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="313">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="314">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="362">
+  <animation>
+   <frame tileid="19" duration="200"/>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="28" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="378">
+  <animation>
+   <frame tileid="375" duration="200"/>
+   <frame tileid="376" duration="200"/>
+   <frame tileid="437" duration="200"/>
+   <frame tileid="438" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="393">
+  <animation>
+   <frame tileid="50" duration="200"/>
+   <frame tileid="53" duration="200"/>
+   <frame tileid="56" duration="200"/>
+   <frame tileid="59" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="409">
+  <animation>
+   <frame tileid="406" duration="200"/>
+   <frame tileid="407" duration="200"/>
+   <frame tileid="468" duration="200"/>
+   <frame tileid="469" duration="200"/>
+  </animation>
+ </tile>
+</tileset>

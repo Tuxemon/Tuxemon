23189f0f5bb3556dc989cbd750c06e7977607400
vXtreniusX <49267339+vXtreniusX@users.noreply.github.com>
2021-07-14 15:48:45 +0200

    Add more (user friendly) commands to the cli (#916)
    * Add add_monster command to the cli
    
    * Added add_item to the cli
    
    * Add set_health to the cli
    
    * Tweak set_health a bit
     - Add exception handling for target_health, resulting in showing the usage text
    
    * Add random_encounter to the cli
    Note: The encounter slug is currently hardcoded to default_encounter
    
    * Add teleport and (unfinished) trainer_battle command
    trainer_battle for some reason returns "AttributeError: 'NoneType' object has no attribute 'npc_slug'"
    
    * Add whereami command to the cli
    
    * Fixing trainer_battle
    
    * Fix trainer_battle
    Note: this command creates a new npc, with the name of the trainer, and after the battle deletes it.
    It might break some events.

--- tuxemon/cli.py
+++ tuxemon/cli.py
@@ -31,6 +31,8 @@ import cmd
 import code
 import logging
 import pprint
+from tuxemon.db import db
+import os
 from threading import Thread
 from typing import TYPE_CHECKING
 
@@ -78,6 +80,10 @@ class CommandLine(cmd.Cmd):
         self.cmd_thread = Thread(target=self.cmdloop)
         self.cmd_thread.daemon = True
         self.cmd_thread.start()
+
+        # For executing actions like add_item,
+        # to avoid defining this variable mutiple times
+        self.action = app.event_engine.execute_action
 
     def emptyline(self) -> bool:
         """If an empty line was entered at the command line, do nothing."""
@@ -164,6 +170,163 @@ class CommandLine(cmd.Cmd):
         print("self.pp.pprint(self.__dict__)")
         self.pp.pprint(self.__dict__)
         code.interact(local=locals())
+
+    def do_add_monster(self, line:str) -> None:
+        """
+        Add monster to the player's party.
+        Parameters:
+            line: arguments
+        """
+        args = line.split(" ")
+        try:
+            monster = args[0]
+            if len( monster.replace(" ", "") ) < 1:
+                raise(ValueError)
+
+            # Check, if level was added as an argument, if not, it will be set to 20
+            try:
+                level = args[1]
+            except:
+                level = 20
+        except:
+            print("Usage: add_monster <slug> [level]")
+            return
+
+        # Check, if the monster exists
+        if not monster in db.database["monster"]:
+            print(f"Monster {monster} doesn't exist!")
+            return
+
+        self.action("add_monster", (monster, level))
+        print(f"Added {monster} to the party!")
+
+
+    def do_add_item(self, line:str) -> None:
+        """
+        Add item to the player's bag.
+        Parameters:
+            line: arguments
+        """
+        args = line.split(" ")
+        try:
+            item = args[0]
+            if len( item.replace(" ", "") ) < 1:
+                raise(ValueError)
+
+            # Check, if the amount of items was added as an argument
+            try:
+                amount = int(args[1])
+            except IndexError:
+                amount = 1
+            except ValueError:
+                # Move the handling one level down
+                raise(ValueError)
+        except:
+            print("Usage: add_item <slug> [amount]")
+            return
+
+        # Check, if the monster exists
+        if not item in db.database["item"]:
+            print(f"Item {item} doesn't exist!")
+            return
+
+        for i in range(amount):
+            self.action("add_item", (item,))
+        self.action("update_inventory")
+        print(f"Added {item} (amount: {amount}) to the bag!")
+
+    def do_set_health(self, line:str) -> None:
+        """
+        Sets the monster's health.
+        Parameters:
+            line: arguments
+        """
+        # Usage info
+        usage_info = "Usage: set_health <target_level> [slot]\ntarget_level must contain a number between 0 and 100\nIf no slot is provided, all monsters in the party will be selected"
+
+        args = line.split(" ")
+
+        # target_level is between 0 and 100, instead of original 0 to 1, to make it more user friendly
+        try:
+            target_health = int(args[0]) / 100
+        except:
+            print(usage_info)
+            return
+
+        try:
+            slot = int(args[1])
+            self.action("set_monster_health",(slot, target_health))
+        except:
+            self.action("set_monster_health", (None, target_health))
+
+    def do_random_encounter(self, line:str) -> None:
+        """
+        Generates random encounter.
+        Parameters:
+            line: ignored
+        """
+        self.action("random_encounter", ("default_encounter",100))
+
+    def do_trainer_battle(self, line:str) -> None:
+        """
+        Generates random encounter.
+        Parameters:
+            line: arguments
+        """
+        print("WARNING: This command is incomplete.")
+        input("Press Enter to continue. ")
+        from tuxemon.event.actions.start_battle import StartBattleActionParameters
+        usage_info = "Usage: trainer_battle <npc_slug>\nor\ntrainer_battle list\nnpc_slug - The npc in the npc database\nlist - List all npcs in the database"
+        args = line.split(" ")
+        try:
+            trainer = args[0]
+
+        except:
+            print(usage_info)
+            return
+
+        if trainer == "list":
+            for i in db.database["npc"]:
+                if "monsters" in db.database["npc"][i]:
+                    print(i)
+        elif trainer in db.database["npc"]:
+            self.action("create_npc", (trainer,7,6))
+            self.action("start_battle", (StartBattleActionParameters(npc_slug=trainer)))
+            self.action("remove_npc", (trainer,))
+
+    def do_teleport(self, line:str) -> None:
+        """
+        Teleports the player to specified coordinates and (optionally) the specified map
+        Parameters:
+            line: arguments
+        """
+        usage_info = "Usage: teleport <x> <y> [map_file]"
+        args = line.split(" ")
+
+        try:
+            x = args[0]
+            y = args[1]
+
+        except:
+            print(usage_info)
+            return
+
+        try:
+            map = args[2]
+        except:
+            map_filename = self.app.event_engine.current_map.data.filename
+            map = os.path.split( map_filename )[1]
+
+        self.action("teleport", (map, x, y))
+
+    def do_whereami(self, line:str) -> None:
+        """
+        Prints the map the player is at to the console
+        Parameters:
+            line: ignored
+        """
+        map = os.path.split(self.app.event_engine.current_map.data.filename)[1]
+        print(map)
 
     def postcmd(self, stop: bool, line: str) -> bool:
         """

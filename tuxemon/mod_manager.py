99263cd0b758d269e26178f3077379be55058867
vXtreniusX <michal2008vxv@gmail.com>
2021-08-26 18:38:35 +0200

    Add mod manager/downloader

---
+++ tuxemon/mod_manager.py
@@ -0,0 +1,64 @@
+import urllib.request
+
+from tuxemon.constants import paths
+import pathlib
+import os
+import json
+import zipfile
+
+class Manager:
+
+    def __init__(self, url="http://127.0.0.1:5000", caching=False):
+        packages_path = os.path.join(paths.CACHE_DIR, "packages")
+
+        self.url = url
+
+        if caching:
+            if pathlib.Path(packages_path).exists():
+                with open(packages_path) as file:
+                    self.packages = json.loads(file.read())
+            else:
+                self.packages = self.update()
+                """with urllib.request.urlopen(url + "/api/packages") as packages:
+                    self.packages = json.loads(packages.read().decode("UTF-8"))"""
+
+                with open(packages_path, "w") as file:
+                    file.write(json.dumps(self.packages, indent=4))
+        else:
+            self.packages = self.update()
+
+    def update(self):
+        """Returns the response from the server"""
+        with urllib.request.urlopen(self.url + "/api/packages") as packages:
+            return json.loads(packages.read().decode("UTF-8"))
+
+    def list_packages(self):
+        """Returns package dictionary, either from the server or the cache"""
+        return self.packages
+
+    def download_package(self, author, name, release, dont_extract=False):
+        """Downloads the specified package"""
+        url = self.url + f"/packages/{author.lower()}/{name}/releases/{release}/download"
+        filename = os.path.join(paths.CACHE_DIR + f"/{author}.{name}.{release}.zip")
+        
+        urllib.request.urlretrieve(url, filename=filename)
+
+
+        outfolder = os.path.join(paths.BASEDIR, "mods", f"{name}-{author.lower()}")
+        if not dont_extract:
+            self.extract(filename, outfolder) 
+            # Adding author name to prevent conflicts,
+            # They will be resolved server side though ¯\_(ツ)_/¯
+            with open(f"{outfolder}/meta.json", "w") as metafile:
+                meta = {
+                    "author": author,
+                    "name": name,
+                    "release": release
+                }
+                metafile.write(json.dumps(meta, indent=4))
+
+    def extract(self,file, outfolder):
+        """Extracts the specified zip archive to the mods directory"""
+        with zipfile.ZipFile(file) as zip_:
+            zip_.extractall(path=os.path.join(paths.BASEDIR, "mods", outfolder))
+

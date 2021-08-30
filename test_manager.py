fbdc592bdffc661b0a2e3c6b7b87e1e6008b1697
vXtreniusX <michal2008vxv@gmail.com>
2021-08-30 15:53:41 +0200

    Revert "Make download_package automatically search for the repository (if not specified)"
    This reverts commit 359d6bb49a82a0f62bb76d76885d7d2aa9dd1f60.
    
    (I hope that I did it right)

--- test_manager.py
+++ test_manager.py
@@ -6,20 +6,18 @@ from pprint import pprint
 ip_list = ["http://127.0.0.1:5000"]
 man = Manager(ip_list)
 
-print("-"*10)
+"""
 pprint(man.list_packages())
 
 man.update_all()
 
 pprint(man.list_packages())
-
+"""
 print("-"*10)
 for i in man.list_packages():
     print(i)
 
 print(man.get_package_repo("package-name"))
-
-man.download_package("package-name", 0)
 """
 for i in ip_list:
     print(i)

4ec5d8f8b3ef2b2f71748afbeb192ab322678286
SandytheLizard <89547310+SandytheLizard@users.noreply.github.com>
2021-09-02 15:21:53 -0700

    Update technique.py

--- tuxemon/technique.py
+++ tuxemon/technique.py
@@ -308,6 +308,10 @@ class Technique:
             max_deviation = stat.get('max_deviation')
             operation = stat.get('operation')
             override = stat.get('overridetofull')
+            if not override:
+                override = False
+            if not max_deviation:
+                max_deviation = False
             basestatvalue = getattr(target, slugdata)
             if max_deviation:
                 value = random.randint(value - max_deviation, value + max_deviation)

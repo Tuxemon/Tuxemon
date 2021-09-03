0d7ce4d442848580a074923881c77544dcc2d398
MasterOChaos <73611161+MasterOChaos@users.noreply.github.com>
2021-09-03 13:11:13 -0400

    Add disclaimer

--- scripts/image_optim_advpng.sh
+++ scripts/image_optim_advpng.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Notice this requires advpng from https://github.com/amadvance/advancecomp
+# You will most likely need to compile it and install it
 find mods -name "*.png" | parallel -j$(nproc) --progress -q advpng -4 -i100 -q -z && find mods -name "*.png" | parallel -j$(nproc) --progress -q advpng -4 -i100 -q -z

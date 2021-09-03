57da3f69594edb0e690c7b308fba991ee7e879fa
MasterOChaos <73611161+MasterOChaos@users.noreply.github.com>
2021-09-03 13:07:55 -0400

    add second run

--- scripts/image_optim_advpng.sh
+++ scripts/image_optim_advpng.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find mods -name "*.png" | parallel -j$(nproc) --progress -q advpng -4 -i100 -q -z
+find mods -name "*.png" | parallel -j$(nproc) --progress -q advpng -4 -i100 -q -z && find mods -name "*.png" | parallel -j$(nproc) --progress -q advpng -4 -i100 -q -z

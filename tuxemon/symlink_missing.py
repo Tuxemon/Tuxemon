57301e5f0925b5f3b3f8560d9d8ba7f699bf51d0
vXtreniusX <michal2008vxv@gmail.com>
2021-09-05 11:27:48 +0200

    Create directory if it doesn't exists

--- tuxemon/symlink_missing.py
+++ tuxemon/symlink_missing.py
@@ -20,7 +20,12 @@ def symlink_missing(target, *sources):
                 # Link the contents on an directory
                 full_path = os.path.join(source, i)
                 if os.path.isdir(full_path):
-                    for a in os.listdir(full_path): # For file in the folder
+                    for a in os.listdir(full_path):  # For file in the folder
+                        try:
+                            os.mkdir(os.path.join(target, i))
+                        except FileExistsError:
+                            pass
+
                         try:
                             os.symlink(
                                 os.path.join(source, i, a),

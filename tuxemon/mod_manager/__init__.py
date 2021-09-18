cf5d91dae76ade19eb5b558bec06a38b26af807e
vXtreniusX <michal2008vxv@gmail.com>
2021-09-18 17:21:43 +0200

    Add ContentDB Compatibility to install_dependencies (seems to be broken)

--- tuxemon/mod_manager/__init__.py
+++ tuxemon/mod_manager/__init__.py
@@ -77,7 +77,7 @@ class Manager:
         """Returns package dictionary, either from the server or the cache"""
         return self.packages
 
-    def download_package(self, author, name, release, repo=None, dont_extract=False, install_deps=True, installed=None):
+    def download_package(self, author, name, release=None, repo=None, dont_extract=False, install_deps=True, installed=None):
         """Downloads the specified package"""
         if repo is None:
             repo = self.get_package_repo(name)
@@ -86,11 +86,23 @@ class Manager:
             if repo[-1] == "/":
                 repo = repo[:-1]
 
+        if release is None:
+            r = requests.get(repo + f"/api/packages/{author}/{name}/releases")
+            print(r.text, author, name)
+            print(repo + f"/api/packages/{author}/{name}/releases")
+            # Get latest release (largest number).
+            latest = 0
+            for i in r.json():
+                if i["id"] > latest:
+                    latest = i["id"]
+            release = latest
+
         # Sanitize name and release
         name = sanitize_paths(name)
         release = sanitize_paths(str(release))
-
+        uwu = input( " ".join([author, name, release, repo, str(dont_extract), str(install_deps), str(installed)]) ) 
         url = str(repo) + f"/packages/{author}/{name}/releases/{str(release)}/download"
+
         filename = os.path.join(paths.CACHE_DIR, f"downloaded_packages/{name}.{release}.zip")
 
         # Apperantly this function is ported from urllib from python2.
@@ -107,7 +119,7 @@ class Manager:
                 for chunk in r.iter_content(chunk_size=8096):
                     file.write(chunk)
                     downloaded_size += 8096
-                    #print(f"{downloaded_size}/{file_size}", int(file_size) - downloaded_size)
+                    print(f"{downloaded_size}/{file_size}", int(file_size) - downloaded_size)
 
         outfolder = os.path.join(paths.BASEDIR, "mods")
 
@@ -126,19 +138,42 @@ class Manager:
             # This function calls download_package, might cause issues
             self.install_dependencies(name, release, repo, dont_extract=dont_extract, done=installed)
 
-    def install_dependencies(self, name, release, repo, dont_extract=False, symlink=True, done=None):
-        """
+    def install_dependencies(self, author, name, repo, symlink=True, **args):
+        """Recursively resolve dependencies and symlink them"""
+        print(author, name, repo)
+        # Request dependencies for specified package
+        r = requests.get(f"https://content.minetest.net/api/packages/{author}/{name}/dependencies/?only_hard=1")
+        #print(r.text, author, name)
+        dep_list = r.json()
+        # Resolve dependencies
+        for dependency in dep_list:
+            for entry in dep_list[dependency]:
+                for package in entry["packages"]:
+                    if os.path.exists(os.path.join(paths.BASEDIR, "mods", package)):
+                        continue
+                    self.download_package(
+                        package.split("/")[0],
+                        package.split("/")[1],
+                        repo=repo,
+                        install_deps=False
+                        
+                    )
+                    
+
+    """def install_dependencies(self, name, release, repo, dont_extract=False, symlink=True, done=None):
+        "#""
         Same as the download_package(), but it includes dependency installing.
         When symlink is True, dependency's files will be linked.
-        """
+        "#""
         name = sanitize_paths(name)
         # Get info
         with open(os.path.join(paths.BASEDIR, "mods", name, "mod.conf")) as file:
             raw_meta = file.read()
+            meta = self.parse_mod_conf(raw_meta)
             
-        #installed = done
-        #if installed is None:
-        #    installed = [name]
+        installed = done
+        if installed is None:
+            installed = [name]
         if "depends" in meta:
             dependencies = meta["depends"]
 
@@ -149,29 +184,33 @@ class Manager:
 
                 if pack in installed: continue
                 
-                self.download_package(pack, release, repo, dont_extract=dont_extract, installed=installed)
+                
+                self.download_package(pack, release, repo=repo, dont_extract=dont_extract, installed=installed)
 
                 # Symlink deps
                 mainfolder = os.path.join(paths.BASEDIR, "mods", name)
                 depfolder = os.path.join(paths.BASEDIR, "mods", pack)
                 symlink_missing(mainfolder, depfolder)
         else: pass
+    """
 
-    def parse_mod_conf(self, content):
-        """
+    """    def parse_mod_conf(self, content):
+        "#""
         Parses the minetest's mod.conf files.
         Returns: dict
-        """
+        "#""
         out = {}
         for line in content.split("\n"):
             # Remove spaces and split into parts
             parts = line\
                 .split(" = ")
+            if len(parts[0]) == 0:
+                continue
             if parts[0] == "depends":
                 element_list = parts[1].split(", ")
                 parts[1] = element_list
             out = {**out, **{parts[0]:parts[1]}}
-        return out
+        return out"""
 
     def get_package_info(self, author, name, repo):
         """Get specified package info. Always downloads the info from the server."""

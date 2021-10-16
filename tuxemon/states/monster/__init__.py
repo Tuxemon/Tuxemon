47694b198c4e89557a073ce5931ce78c254f299c
vnmabus <vnmabus@gmail.com>
2021-10-16 18:21:16 +0200

    Type monster state.

--- tuxemon/states/monster/__init__.py
+++ tuxemon/states/monster/__init__.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 import pygame
 
-from tuxemon.compat import Rect
+from pygame.rect import Rect
 from tuxemon import prepare, graphics
 from tuxemon import tools
 from tuxemon.menu.interface import HpBar, ExpBar, MenuItem
@@ -8,10 +9,13 @@ from tuxemon.menu.menu import Menu
 from tuxemon.session import local_session
 from tuxemon.ui.draw import GraphicBox
 from tuxemon.ui.text import draw_text, TextArea
+from typing import Any, Optional, Generator
+from tuxemon.monster import Monster
 
 
-class MonsterMenuState(Menu):
-    """A class to create monster menu objects.
+class MonsterMenuState(Menu[Optional[Monster]]):
+    """
+    A class to create monster menu objects.
 
     The monster menu allows you to view monsters in your party,
     teach them moves, and switch them both in and out of combat.
@@ -20,12 +24,12 @@ class MonsterMenuState(Menu):
     background_filename = "gfx/ui/monster/monster_menu_bg.png"
     draw_borders = False
 
-    def startup(self, **kwargs):
+    def startup(self, **kwargs: Any) -> None:
         super().startup(**kwargs)
 
         # make a text area to show messages
         self.text_area = TextArea(self.font, self.font_color, (96, 96, 96))
-        self.text_area.rect = Rect(tools.scale_sequence([20, 80, 80, 100]))
+        self.text_area.rect = Rect(tools.scale_sequence((20, 80, 80, 100)))
         self.sprites.add(self.text_area, layer=100)
 
         # Set up the border images used for the monster slots
@@ -51,26 +55,36 @@ class MonsterMenuState(Menu):
         for monster in local_session.player.monsters:
             monster.load_sprites()
 
-    def animate_monster_down(self):
+    def animate_monster_down(self) -> None:
         ani = self.animate(
-            self.monster_portrait.rect, y=-tools.scale(5), duration=1, transition="in_out_quad", relative=True
+            self.monster_portrait.rect,
+            y=-tools.scale(5),
+            duration=1,
+            transition="in_out_quad",
+            relative=True,
         )
         ani.callback = self.animate_monster_up
 
-    def animate_monster_up(self):
+    def animate_monster_up(self) -> None:
         ani = self.animate(
-            self.monster_portrait.rect, y=tools.scale(5), duration=1, transition="in_out_quad", relative=True
+            self.monster_portrait.rect,
+            y=tools.scale(5),
+            duration=1,
+            transition="in_out_quad",
+            relative=True,
         )
         ani.callback = self.animate_monster_down
 
-    def calc_menu_items_rect(self):
+    def calc_menu_items_rect(self) -> Rect:
         width, height = self.rect.size
         left = width // 2.25
         top = height // 12
-        width /= 2
+        width //= 2
         return Rect(left, top, width, height - top * 2)
 
-    def initialize_items(self):
+    def initialize_items(
+        self,
+    ) -> Generator[MenuItem[Optional[Monster]], None, None]:
         # position the monster portrait
         try:
             monster = local_session.player.monsters[self.selected_index]
@@ -80,16 +94,21 @@ class MonsterMenuState(Menu):
 
         # position and animate the monster portrait
         width, height = prepare.SCREEN_SIZE
-        self.monster_portrait.rect = image.get_rect(centerx=width // 4, top=height // 12)
+        self.monster_portrait.rect = image.get_rect(
+            centerx=width // 4,
+            top=height // 12,
+        )
         self.sprites.add(self.monster_portrait)
         self.animations.empty()
         self.animate_monster_down()
 
         width = prepare.SCREEN_SIZE[0] // 2
-        height = prepare.SCREEN_SIZE[1] // (local_session.player.party_limit * 1.5)
+        height = prepare.SCREEN_SIZE[1] // int(
+            local_session.player.party_limit * 1.5,
+        )
 
         # make 6 slots
-        for i in range(local_session.player.party_limit):
+        for _ in range(local_session.player.party_limit):
             rect = Rect(0, 0, width, height)
             surface = pygame.Surface(rect.size, pygame.SRCALPHA)
             item = MenuItem(surface, None, None, None)
@@ -97,48 +116,73 @@ class MonsterMenuState(Menu):
 
         self.refresh_menu_items()
 
-    def on_menu_selection(self, menu_item):
+    def on_menu_selection(
+        self,
+        menu_item: MenuItem[Optional[Monster]],
+    ) -> None:
         pass
 
-    def render_monster_slot(self, surface, rect, monster, in_focus):
+    def render_monster_slot(
+        self,
+        surface: pygame.surface.Surface,
+        rect: Rect,
+        monster: Optional[Monster],
+        in_focus: bool,
+    ) -> pygame.surface.Surface:
         filled = monster is not None
         border = self.determine_border(in_focus, filled)
         border.draw(surface)
-        if filled:
+        if monster is not None:
             self.draw_monster_info(surface, monster, rect)
         return surface
 
-    def is_valid_entry(self, monster):
-        """Used to determine if a given monster should be selectable.
-            When other code creates a MonsterMenu, it should overwrite this method
-            to suit it's needs.
-        :param monster:
-        :return:
+    def is_valid_entry(self, monster: Optional[Monster]) -> bool:
+        """
+        Used to determine if a given monster should be selectable.
+
+        When other code creates a MonsterMenu, it should overwrite this method
+        to suit it's needs.
+
+        Parameters:
+            monster: The monster corresponding to the menu item, if any.
+
         """
         return monster is not None
 
-    def refresh_menu_items(self):
-        """Used to render slots after their 'focus' flags change
-
-        :return:
-        """
+    def refresh_menu_items(self) -> None:
+        """Used to render slots after their 'focus' flags change."""
 
         for index, item in enumerate(self.menu_items):
+
+            monster: Optional[Monster]
             try:
                 monster = local_session.player.monsters[index]
             except IndexError:
                 monster = None
             item.game_object = monster
 
-            item.enabled = (monster is not None) and self.is_valid_entry(item.game_object)
+            item.enabled = (
+                (monster is not None)
+                and self.is_valid_entry(item.game_object)
+            )
             item.image.fill((0, 0, 0, 0))
             item.in_focus = (index == self.selected_index) and item.enabled
-            self.render_monster_slot(item.image, item.image.get_rect(), item.game_object, item.in_focus)
+            self.render_monster_slot(
+                item.image,
+                item.image.get_rect(),
+                item.game_object,
+                item.in_focus,
+            )
 
-    def draw_monster_info(self, surface, monster, rect):
+    def draw_monster_info(
+        self,
+        surface: pygame.surface.Surface,
+        monster: Monster,
+        rect: Rect,
+    ) -> None:
         # position and draw hp bar
         hp_rect = rect.copy()
-        left = rect.width * 0.6
+        left = int(rect.width * 0.6)
         right = rect.right - tools.scale(4)
         hp_rect.width = right - left
         hp_rect.left = left
@@ -155,7 +199,12 @@ class MonsterMenuState(Menu):
 
         # draw the level info
         text_rect.top = rect.bottom - tools.scale(7)
-        draw_text(surface, "  Lv " + str(monster.level), text_rect, font=self.font)
+        draw_text(
+            surface,
+            "  Lv " + str(monster.level),
+            text_rect,
+            font=self.font,
+        )
 
         # draw any status icons
         # TODO: caching or something, idk
@@ -163,10 +212,13 @@ class MonsterMenuState(Menu):
         for index, status in enumerate(monster.status):
             if status.icon:
                 image = graphics.load_and_scale(status.icon)
-                pos = (rect.width * 0.4) + (index * tools.scale(32)), rect.y + tools.scale(5)
+                pos = (
+                    (rect.width * 0.4) + (index * tools.scale(32)),
+                    rect.y + tools.scale(5),
+                )
                 surface.blit(image, pos)
 
-    def determine_border(self, selected, filled):
+    def determine_border(self, selected: bool, filled: bool) -> GraphicBox:
         if selected:
             return self.monster_slot_border["active"]
         elif filled:
@@ -174,7 +226,7 @@ class MonsterMenuState(Menu):
         else:
             return self.monster_slot_border["empty"]
 
-    def on_menu_selection_change(self):
+    def on_menu_selection_change(self) -> None:
         try:
             monster = local_session.player.monsters[self.selected_index]
             image = monster.sprites["front"]

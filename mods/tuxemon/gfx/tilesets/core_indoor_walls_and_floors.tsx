9983aee18253272722301708c7a027be1cf4fdf2
rubberduck <sebastian.platen@posteo.de>
2022-04-21 18:41:12 +0200

    cleanup

--- mods/tuxemon/gfx/tilesets/core_indoor_walls_and_floors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="core_indoor_walls_and_floors" tilewidth="16" tileheight="16" tilecount="3864" columns="46">
- <image source="core_indoor_walls_and_floors.png" width="736" height="1344"/>
-</tileset>

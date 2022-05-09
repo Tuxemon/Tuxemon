7459675b00099afaaff12fc6adca138949417f5f
Leif Theden <leif.theden@gmail.com>
2022-05-09 09:25:36 -0500

    disable formatter stuff that would be difficult to use on windows

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -4,7 +4,7 @@
 # consider using before opening a PR
 
 pip install -U black autoflake pyupgrade isort
-find tuxemon/ -name "*.py" -type f | parallel pyupgrade --py38 --keep-runtime-typing
-autoflake -r -i --remove-all-unused-imports --exclude "*/__init__.py" tuxemon/
+# find tuxemon/ -name "*.py" -type f | parallel pyupgrade --py38 --keep-runtime-typing
+# autoflake -r -i --remove-all-unused-imports --exclude "*/__init__.py" tuxemon/
 isort tuxemon tests
 black -t py38 -l 79 tuxemon tests

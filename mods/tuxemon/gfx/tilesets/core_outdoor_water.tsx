2bf552272c2f1476e3cb0cd622a7461bc13b03ab
rubberduck <sebastian.platen@posteo.de>
2022-05-13 21:42:04 +0200

    more work on outdoor tilesets including new water tiles and update on the prototype tileset

---
+++ mods/tuxemon/gfx/tilesets/core_outdoor_water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="core_outdoor_water" tilewidth="16" tileheight="16" tilecount="8192" columns="64">
+ <image source="core_outdoor_water.png" width="1024" height="2048"/>
+</tileset>

7f093134034c68618f2d2fffb8e16707895e2fb8
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-05-14 13:14:00 +0200

    precompile pattern

--- tuxemon/graphics.py
+++ tuxemon/graphics.py
@@ -262,10 +262,10 @@ def animation_frame_files(
 
     """
     frames = list()
-    pattern = fr"{name}[0-9]*\..*"
+    pattern = re.compile(fr"{name}[0-9]*\..*")
     # might be slow on large folders
     for filename in os.listdir(directory):
-        if re.match(pattern, filename):
+        if pattern.match(filename):
             frames.append(os.path.join(directory, filename))
     frames.sort()
     return frames

4e1e6d1c283145dccb468d1482fa807f2bda3e3f
Andy Bylicki <andymenderunix@gmail.com>
2022-08-30 09:03:36 +0200

    Refactor download_technique_animations script (#1268)

--- scripts/download_technique_animations.py
+++ scripts/download_technique_animations.py
@@ -9,13 +9,18 @@ If the file names have changed from the wiki, the technique
 json files will need to be updated as well.
 """
 from urllib.parse import urljoin
+import os.path
 
 import requests
 from PIL import Image
 from lxml import html
 
+FRAME_SIZE = 64
+WIKI_URL = "https://wiki.tuxemon.org/index.php?title=Category:Technique_Animation"
 
-def download(url, filename):
+# TODO: Is the return here needed or should be boolean to indicate success/fail?
+def download_bytes(url: str, filename: str) -> str:
+    """Downloads a stream of bytes from a given URL to a file defined by 'filename'."""
     req = requests.get(url)
     with open(filename, "wb") as fp:
         for chunk in req.iter_content(chunk_size=1024):
@@ -24,31 +29,35 @@ def download(url, filename):
     return filename
 
 
-def process_filename(filename):
-    filename = filename[:-4]
-    filename = filename.strip("0123456789_")
-    filename = filename.lower()
-    return filename
+def process_filename(filename: str) -> str:
+    """Extract base filename from an animation file path."""
+    cleaned_filename = os.path.splitext(filename)[0]
+    cleaned_filename = cleaned_filename.strip("0123456789_")
+    cleaned_filename = cleaned_filename.lower()
+    return cleaned_filename
 
 
-def process_gif(filename):
-    image = Image.open(filename)
-    if not image.is_animated:
-        print("...not animated, skipped")
-        return
-    if not image.width == image.height == 64:
-        print("...is not 64x64, skipped")
-        return
-    base_name = process_filename(filename)
-    for frame in range(0, image.n_frames):
-        frame_filename = f"{base_name}{frame:02}.png"
-        print(f"saving {filename}:{frame_filename} {frame}/{image.n_frames - 1}")
-        image.seek(frame)
-        image.save(frame_filename, optimize=True)
+def process_gif(filename: str) -> None:
+    """Extract individual animation frames as PNG from a GIF."""
+    with Image.open(filename) as image:
+        if not image.is_animated:
+            print(f"{filename} is not animated, skipped")
+            return
+        if not image.width == image.height == FRAME_SIZE:
+            print(f"{filename} is not {FRAME_SIZE}x{FRAME_SIZE}, skipped")
+            return
+        base_name = process_filename(filename)
+        for frame in range(0, image.n_frames):
+            frame_filename = f"{base_name}{frame:02}.png"
+            print(f"saving {filename}:{frame_filename} {frame}/{image.n_frames - 1}")
+            image.seek(frame)
+            image.save(frame_filename, optimize=True)
 
 
-def download_technique_animations(wiki_url):
-    print("getting page source...")
+# TODO: Is passing the Wiki URL needed?
+def download_technique_animations(wiki_url: str) -> None:
+    """Download technique animation frames from the Tuxemon Wiki."""
+    print(f"Getting page source for URL: {wiki_url}")
     source = requests.get(wiki_url)
     tree = html.fromstring(source.content)
     elements = tree.xpath("//li[@class='gallerybox']//img")
@@ -57,10 +66,9 @@ def download_technique_animations(wiki_url):
         url = urljoin(wiki_url, element.get("src"))
         filename = url.split('/')[-1]
         print(f"{index}/{len(elements)} downloading {filename}")
-        download(url, filename)
+        download_bytes(url, filename)
         process_gif(filename)
 
 
 if __name__ == "__main__":
-    wiki_url = "https://wiki.tuxemon.org/index.php?title=Category:Technique_Animation"
-    download_technique_animations(wiki_url)
+    download_technique_animations(WIKI_URL)

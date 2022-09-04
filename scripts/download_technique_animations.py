90758ad8a3693ad8adf2b1938b44d91dcb3bea5d
Andy Mender <andymenderunix@gmail.com>
2022-09-04 23:22:23 +0200

    Fix whitespace and remove needless string typehints

--- scripts/download_technique_animations.py
+++ scripts/download_technique_animations.py
@@ -54,20 +54,20 @@ def process_animation_name(filename: str) -> str:
 
 def download_animation_credits(gif_page_url: str) -> str:
     """Extract credits text from a GIF animation subpage, from the 'Comments' section."""
-    gif_filename: str = gif_page_url.split("File:")[-1]
-    animation_name: str = process_animation_name(process_filename(gif_filename))
+    gif_filename = gif_page_url.split("File:")[-1]
+    animation_name = process_animation_name(process_filename(gif_filename))
 
     gifpage_source = requests.get(gif_page_url)
     gifpage_tree = html.fromstring(gifpage_source.content)
     comment_blocks = gifpage_tree.xpath("//table[@class='wikitable filehistory']//td[@dir='ltr']")
 
-    credits_text =""
+    credits_text = ""
     for comment_row in comment_blocks: 
         # Some comment entries are empty
         if len(comment_row) > 0:
             for comment_entry in comment_row:
                 # Assemble credits and skip the "Category:" blocks   
-                comment_tail: str = comment_entry.tail
+                comment_tail = comment_entry.tail
                 if comment_tail is not None and not comment_tail.startswith("Category:"):
                     credits_text += comment_entry.tail.strip()
 
@@ -86,10 +86,10 @@ def gif_to_frames(filepath: str) -> None:
             print(f"{filepath} is not {FRAME_SIZE}x{FRAME_SIZE}, skipped")
             return
   
-        base_name: str = process_filename(filepath)
-        animation_name: str = process_animation_name(base_name)
+        base_name = process_filename(filepath)
+        animation_name = process_animation_name(base_name)
         for frame in range(0, image.n_frames):
-            frame_filename: str = os.path.join(ANIMATION_DIR, f"{base_name}{frame:02}.png")
+            frame_filename = os.path.join(ANIMATION_DIR, f"{base_name}{frame:02}.png")
             print(f"Generating animation frames for '{animation_name}': {frame_filename} - {frame}/{image.n_frames - 1}")
             image.seek(frame)
             image.save(frame_filename, optimize=True)
@@ -113,18 +113,18 @@ def download_technique_animations(wiki_url: str) -> None:
             print("", file=credits_file)
             for index, element in enumerate(elements, start=1):
                 # Download animation GIF and convert to frame PNGs
-                gif_url: str = urljoin(animations_url, element[0].get("src"))
-                filename: str = gif_url.split('/')[-1]
+                gif_url = urljoin(animations_url, element[0].get("src"))
+                filename = gif_url.split('/')[-1]
                 print(f"Downloading animation [{index}/{len(elements)}] - {filename}")
 
-                temppath: str = os.path.join(tmpdirname, filename)
+                temppath = os.path.join(tmpdirname, filename)
                 download_bytes(gif_url, temppath)
                 gif_to_frames(temppath)
 
                 # Download credits from GIF subpage URL
                 print(f"Downloading animation credits [{index}/{len(elements)}] - {filename}")
-                gif_page_url: str = urljoin(wiki_url, element.get("href"))
-                credits_record: str = download_animation_credits(gif_page_url)
+                gif_page_url = urljoin(wiki_url, element.get("href"))
+                credits_record = download_animation_credits(gif_page_url)
                 print(credits_record, file=credits_file)
                 print("", file=credits_file)
  

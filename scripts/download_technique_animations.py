d95d0180c4ebd6b5defef47171cb6e0d7994c995
Andy Mender <andymenderunix@gmail.com>
2022-09-05 21:52:34 +0200

    Run 'isort' and 'black' formatters

--- scripts/download_technique_animations.py
+++ scripts/download_technique_animations.py
@@ -14,14 +14,16 @@ import tempfile
 from urllib.parse import urljoin
 
 import requests
-from PIL import Image
 from lxml import html
+from PIL import Image
 
 FRAME_SIZE = 64
 WIKI_URL = "https://wiki.tuxemon.org"
 
 TUXEMON_ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
-ANIMATION_DIR = TUXEMON_ROOT_DIR.joinpath(pathlib.Path("mods/tuxemon/animations/technique"))
+ANIMATION_DIR = TUXEMON_ROOT_DIR.joinpath(
+    pathlib.Path("mods/tuxemon/animations/technique")
+)
 CREDITS_TEMPLATE = """* ["{animation_name}"]({animation_url})
 {credits_text}"""
 CREDITS_FILENAME = "TECHNIQUE_ANIMATION_CREDITS.md"
@@ -49,7 +51,9 @@ def process_filename(filepath: str) -> str:
 
 def process_animation_name(filename: str) -> str:
     """Convert filename of format 'file_name' to a capitalized 'File Name' animation name/title."""
-    animation_name = " ".join([name_part.capitalize() for name_part in filename.split("_")])
+    animation_name = " ".join(
+        [name_part.capitalize() for name_part in filename.split("_")]
+    )
     return animation_name
 
 
@@ -60,20 +64,28 @@ def download_animation_credits(gif_page_url: str) -> str:
 
     gifpage_source = requests.get(gif_page_url)
     gifpage_tree = html.fromstring(gifpage_source.content)
-    comment_blocks = gifpage_tree.xpath("//table[@class='wikitable filehistory']//td[@dir='ltr']")
+    comment_blocks = gifpage_tree.xpath(
+        "//table[@class='wikitable filehistory']//td[@dir='ltr']"
+    )
 
     credits_text = ""
-    for comment_row in comment_blocks: 
+    for comment_row in comment_blocks:
         # Some comment entries are empty
         if len(comment_row) > 0:
             for comment_entry in comment_row:
-                # Assemble credits and skip the "Category:" blocks   
+                # Assemble credits and skip the "Category:" blocks
                 comment_tail = comment_entry.tail
-                if comment_tail is not None and not comment_tail.startswith("Category:"):
+                if comment_tail is not None and not comment_tail.startswith(
+                    "Category:"
+                ):
                     credits_text += comment_entry.tail.strip()
 
     credits_text = credits_text or "Unknown animation source/author"
-    credits_record = CREDITS_TEMPLATE.format(animation_name=animation_name, animation_url=gif_page_url, credits_text=credits_text)
+    credits_record = CREDITS_TEMPLATE.format(
+        animation_name=animation_name,
+        animation_url=gif_page_url,
+        credits_text=credits_text,
+    )
     return credits_record
 
 
@@ -86,12 +98,16 @@ def gif_to_frames(filepath: str) -> None:
         if not image.width == image.height == FRAME_SIZE:
             print(f"{filepath} is not {FRAME_SIZE}x{FRAME_SIZE}, skipped")
             return
-  
+
         base_name = process_filename(filepath)
         animation_name = process_animation_name(base_name)
         for frame in range(0, image.n_frames):
-            frame_filename = os.path.join(ANIMATION_DIR, f"{base_name}{frame:02}.png")
-            print(f"Generating animation frames for '{animation_name}': {frame_filename} - {frame}/{image.n_frames - 1}")
+            frame_filename = os.path.join(
+                ANIMATION_DIR, f"{base_name}{frame:02}.png"
+            )
+            print(
+                f"Generating animation frames for '{animation_name}': {frame_filename} - {frame}/{image.n_frames - 1}"
+            )
             image.seek(frame)
             image.save(frame_filename, optimize=True)
 
@@ -108,27 +124,33 @@ def download_technique_animations(wiki_url: str) -> None:
 
     with tempfile.TemporaryDirectory() as tmp_dirname:
 
-        elements = anim_tree.xpath("//li[@class='gallerybox']//a[@class='image']")
+        elements = anim_tree.xpath(
+            "//li[@class='gallerybox']//a[@class='image']"
+        )
         with open(CREDITS_FILENAME, "w") as credits_file:
             print("### Technique Animations", file=credits_file)
             print("", file=credits_file)
             for index, element in enumerate(elements, start=1):
                 # Download animation GIF and convert to frame PNGs
                 gif_url = urljoin(animations_url, element[0].get("src"))
-                filename = gif_url.split('/')[-1]
-                print(f"Downloading animation [{index}/{len(elements)}] - {filename}")
+                filename = gif_url.split("/")[-1]
+                print(
+                    f"Downloading animation [{index}/{len(elements)}] - {filename}"
+                )
 
                 temppath = os.path.join(tmp_dirname, filename)
                 download_bytes(gif_url, temppath)
                 gif_to_frames(temppath)
 
                 # Download credits from GIF subpage URL
-                print(f"Downloading animation credits [{index}/{len(elements)}] - {filename}")
+                print(
+                    f"Downloading animation credits [{index}/{len(elements)}] - {filename}"
+                )
                 gif_page_url = urljoin(wiki_url, element.get("href"))
                 credits_record = download_animation_credits(gif_page_url)
                 print(credits_record, file=credits_file)
                 print("", file=credits_file)
- 
+
 
 if __name__ == "__main__":
     download_technique_animations(WIKI_URL)

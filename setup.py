fab2282e670e212f6cb56ff07a856e34c69713bd
meirw770 <77470808+meirw770@users.noreply.github.com>
2022-10-18 11:18:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,19 +18,7 @@ class InstallAndBuildTranslations(install):
         super().__init__(*args, **kwargs)
         # build_translations()
 
-"""
-# Find all the python modules
-modules = []
-matches = []
-for root, dirnames, filenames in os.walk("tuxemon"):
-    for filename in fnmatch.filter(filenames, "__init__.py"):
-        matches.append(os.path.join(root, filename))
 
-for match in matches:
-    match = match.replace(os.sep + "__init__.py", "")
-    match = match.replace(os.sep, ".")
-    modules.append(match)
-"""
 # Get the version from the README file.
 with open("README.md", "r") as f:
     VERSION = f.readline().split(" ")[-1].replace("\n", "")
@@ -39,7 +27,7 @@ with open("README.md", "r") as f:
 with open("requirements.txt", "r") as f:
     REQUIREMENTS = f.read().splitlines()
 
-    
+# Configure the setuptools    
 setup(
     name='tuxemon',
     version='1.0',
@@ -48,35 +36,3 @@ setup(
     packages=find_packages(),
 
 )
-
-"""
-# Configure the setuptools
-setup(
-    name="tuxemon",
-    version=VERSION,
-    description="Open source monster-fighting RPG",
-    author="William Edwards",
-    author_email="shadowapex@gmail.com",
-    maintainer="Tuxemon",
-    maintainer_email="info@tuxemon.org",
-    url="https://www.tuxemon.org",
-    include_package_data=True,
-    packages=modules,
-    license="GPLv3",
-    long_description="https://github.com/Tuxemon/Tuxemon",
-    install_requires=REQUIREMENTS,
-    python_requires=">=3.8",
-    entry_points={"gui_scripts": ["tuxemon = tuxemon.__main__:main"]},
-    classifiers=[
-        "Intended Audience :: End Users/Desktop",
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Games/Entertainment",
-        "Topic :: Games/Entertainment :: Role-Playing",
-    ],
-    cmdclass={"install": InstallAndBuildTranslations},
-)
-"""

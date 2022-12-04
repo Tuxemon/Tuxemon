17cf72c5fa76acb04b3b538bbe441cba7328ce01
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2022-12-04 12:29:28 +0100

    item location

---
+++ tuxemon/item/conditions/location_inside.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from tuxemon.item.itemcondition import ItemCondition
+from tuxemon.monster import Monster
+
+
+@dataclass
+class LocationInsideCondition(ItemCondition):
+    """
+    Checks against the location type the player's in.
+
+    Accepts "inside" or "outside"
+
+    """
+
+    name = "location_inside"
+    location_inside: str
+
+    def test(self, target: Monster) -> bool:
+        if self.location_inside == "inside":
+            if self.session.client.map_inside:
+                return True
+            else:
+                return False
+        elif self.location_inside == "outside":
+            if self.session.client.map_inside:
+                return False
+            else:
+                return True

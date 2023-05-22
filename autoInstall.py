b5b03fbee4ae95e64d23b84180d51b683343bee1
Ahmed Al-Tubuly <74717045+Pilot63@users.noreply.github.com>
2023-05-22 11:02:35 +0200

    autoInstall (file reader used)

--- autoInstall.py
+++ autoInstall.py
@@ -12,7 +12,6 @@ reqtemp = req.read()
 reqlist = reqtemp.split("\n")
 print(reqlist)
 
-#requirements = ["babel", "failureHaHa", "cbor", "neteria", "pillow", "pygame-ce==2.2.0", "pyscroll==2.30", "pytmx==3.31", "requests>=2.19.1", "natsort", "PyYAML>=5.4.1", "prompt_toolkit", "pygame-menu-ce>=4.4.1", "pydantic>=1.9.1"]    
 
 listpos = 0
 while True:

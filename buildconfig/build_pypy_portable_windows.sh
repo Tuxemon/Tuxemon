2f5ee093e1a3096a0a7cfccc76ba9f31ec84e3d9
NeonFlames <80435399+NeonFlames@users.noreply.github.com>
2023-10-07 10:04:12 -0400

    Update the portable Linux and Windows pypy builds

--- buildconfig/build_pypy_portable_windows.sh
+++ buildconfig/build_pypy_portable_windows.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
-# currently needs a compiler to build pygame.  needs to be tested on windows.
-build_folder="tuxemon"
-pypy_build="pypy3.7-v7.3.3-win32"
+
+#############  NOTICE  ##############
+# Tested in a debian stable container
+
+download () {
+  command curl -L -o $1 $2
+}
+
+apt install -y wine curl coreutils p7zip-full
+
+root_folder="$(dirname $(readlink -f $0))/../"
+pypy_vers="pypy3.9"
+pypy_build="pypy3.9-v7.3.13-win64"
 pypy_archive="$pypy_build.zip"
-mkdir -p build/pypy-win32
-cd build/pypy-win32
-[ -e $pypy_archive ] || wget -q https://downloads.python.org/pypy/$pypy_archive
-[ -e $build_folder ] || {
- unzip $pypy_archive
- mv $pypy_build $build_folder
-}
-cd $build_folder
-cp -a ../../../tuxemon site-packages/
-cp -a ../../../mods .
-wine pypy3.exe -m ensurepip
-wine pypy3.exe -m pip install -r ../../../requirements.txt
-[ -e docs ] || mkdir docs
-[ -e README.rst ] && mv README.rst docs
-[ -e LICENSE ] && mv LICENSE docs/PYPY_LICENSE
-cp ../../../LICENSE docs/TUXEMON_LICENSE
-cp ../../../run_tuxemon.py .
-cp ../../../CONTRIBUTING.md .
-cp ../../../CREDITS.md .
-cp ../../../README.md .
-cp ../../../SPYDER_README.md .
+
+if [[ -e build/pypy-win-64bit ]]; then rm -rf build/pypy-win-64bit; fi
+mkdir -p build/pypy-win-64bit
+cd build/
+
+if ! [[ -e $pypy_archive ]]; then
+  download $pypy_archive https://downloads.python.org/pypy/$pypy_archive
+fi
+
+cd pypy-win-64bit
+
+7z x ../$pypy_archive
+mv $pypy_build pypy
+
+wine64 pypy/pypy.exe -m ensurepip
+wine64 pypy/pypy.exe -m pip install pygame-ce==2.2.0
+wine64 pypy/pypy.exe -m pip install -r "$root_folder/requirements.txt"
+cp -a $root_folder/tuxemon pypy/Lib/tuxemon
+cp -a $root_folder/mods .
+cp $root_folder/LICENSE .
+cp $root_folder/run_tuxemon.py .
+cp $root_folder/CONTRIBUTING.md .
+cp $root_folder/CREDITS.md .
+cp $root_folder/README.md .
+cp $root_folder/SPYDER_README.md .
 find . -name "*pyc" -delete
-[ -e run_tuxemon.bat ] || {
- cat > run_tuxemon.bat << EOF
-pypy3.exe run_tuxemon.py
+
+cat << EOF >> Tuxemon.bat
+SETCONSOLE /Hide
+%~dp0\\pypy\\bin\\pypy.exe %~dp0\\run_tuxemon.py
 EOF
- chmod +x run_tuxemon.sh
-}
-[ -e README_WINDOWS.txt ] || {
- cat > README_WINDOWS.txt << EOF
-If the game does not start, you may need the following to be installed:
+chmod a+x Tuxemon.bat
+
+cat << EOF >> README_WINDOWS.txt
+If the game does not start you may need to install vcredist:
 https://www.microsoft.com/en-us/download/details.aspx?id=52685
 EOF
-}
+

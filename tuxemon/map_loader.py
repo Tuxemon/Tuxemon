8dc73ee51965f11a0ce8cce469a035363adf0878
Jaskrendix <tur3a.giorgio.menzi@gmail.com>
2023-11-18 16:58:34 +0100

    fix yaml 2

--- tuxemon/map_loader.py
+++ tuxemon/map_loader.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2014-2023 William Edwards <shadowapex@gmail.com>, Benjamin Bean <superman2k5@gmail.com>
 import logging
+import uuid
 from collections.abc import Generator, Iterator, Mapping
 from math import cos, pi, sin
 from typing import Any, Optional
@@ -66,23 +67,30 @@ class YAMLEventLoader:
             path: Path to the file.
 
         """
+        yaml_data: dict[str, dict[str, dict[str, Any]]] = {}
+
         with open(path) as fp:
             yaml_data = yaml.load(fp.read(), Loader=yaml.SafeLoader)
 
         for name, event_data in yaml_data["events"].items():
+            _id = uuid.uuid4().int
             conds = []
             acts = []
-            x = event_data.get("x")
-            y = event_data.get("y")
-            w = event_data.get("width")
-            h = event_data.get("height")
+            x = event_data.get("x", 0)
+            y = event_data.get("y", 0)
+            w = event_data.get("width", 1)
+            h = event_data.get("height", 1)
             event_type = event_data.get("type")
 
-            for value in event_data.get("actions", []):
+            for key, value in enumerate(
+                event_data.get("actions", []), start=1
+            ):
                 act_type, args = parse_action_string(value)
-                action = MapAction(act_type, args, None)
+                action = MapAction(act_type, args, f"act{str(key*10)}")
                 acts.append(action)
-            for value in event_data.get("conditions", []):
+            for key, value in enumerate(
+                event_data.get("conditions", []), start=1
+            ):
                 operator, cond_type, args = parse_condition_string(value)
                 condition = MapCondition(
                     type=cond_type,
@@ -92,10 +100,10 @@ class YAMLEventLoader:
                     width=w,
                     height=h,
                     operator=operator,
-                    name="",
+                    name=f"cond{str(key*10)}",
                 )
                 conds.append(condition)
-            for value in event_data.get("behav", []):
+            for key, value in enumerate(event_data.get("behav", []), start=1):
                 behav_type, args = parse_behav_string(value)
                 if behav_type == "talk":
                     condition = MapCondition(
@@ -106,13 +114,13 @@ class YAMLEventLoader:
                         width=w,
                         height=h,
                         operator="is",
-                        name="",
+                        name=f"behav{str(key*10)}",
                     )
                     conds.insert(0, condition)
                     action = MapAction(
                         type="npc_face",
                         parameters=[args[0], "player"],
-                        name="",
+                        name=f"behav{str(key*10)}",
                     )
                     acts.insert(0, action)
                 else:
@@ -130,7 +138,7 @@ class YAMLEventLoader:
                 )
                 conds.append(cond_data)
 
-            yield EventObject(name, name, x, y, w, h, conds, acts)
+            yield EventObject(_id, name, x, y, w, h, conds, acts)
 
 
 class TMXMapLoader:

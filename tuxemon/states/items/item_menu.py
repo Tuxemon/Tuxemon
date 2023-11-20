630938c38c9963ea6499142de3a217e8cdcc1d65
Jaskrendix <tur3a.giorgio.menzi@gmail.com>
2023-11-20 16:21:26 +0100

    tech various

--- tuxemon/states/items/item_menu.py
+++ tuxemon/states/items/item_menu.py
@@ -156,20 +156,7 @@ class ItemMenuState(Menu[Item]):
             result = item.use(player, monster)
             self.client.pop_state()  # pop the monster screen
             self.client.pop_state()  # pop the item screen
-            # check effects, some don't need the show_item_result_as_dialog
-            # (e.g. learn_mm, etc)
-            for t in item.effects:
-                if t.name == "learn_mm" or t.name == "learn_tm":
-                    if result["success"]:
-                        player.check_max_moves(local_session, monster)
-                    else:
-                        tools.show_item_result_as_dialog(
-                            local_session, item, result
-                        )
-                else:
-                    tools.show_item_result_as_dialog(
-                        local_session, item, result
-                    )
+            tools.show_item_result_as_dialog(local_session, item, result)
 
         def use_item_no_monster(itm: Item) -> None:
             player = local_session.player

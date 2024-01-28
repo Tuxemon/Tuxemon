a4e3c323088c7306021bca4cee0ba8d87ffc3fb6
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2024-01-28 04:20:16 +0100

    battle menu (#2214)
    * battle menu
    
    * battle menu

--- tuxemon/technique/effects/area.py
+++ tuxemon/technique/effects/area.py
@@ -41,12 +41,12 @@ class AreaEffect(TechEffect):
             # 2 vs 2, damage both monsters
             if player.max_position > 1:
                 monsters: Sequence[Monster] = []
-                human = combat.monsters_in_play_human
-                opponent = combat.monsters_in_play_ai
-                if player.isplayer:
-                    monsters = opponent
+                _right = combat.monsters_in_play_right
+                _left = combat.monsters_in_play_left
+                if user in _right:
+                    monsters = _left
                 else:
-                    monsters = human
+                    monsters = _right
                 for mon in monsters:
                     mon.current_hp -= damage
                     combat.enqueue_damage(user, mon, damage)

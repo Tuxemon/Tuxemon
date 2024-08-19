01d53e7489ecb7edcdd6fd62ea210603c2fbc004
Jaskrendix <tur3a.giorgio.menzi@gmail.com>
2024-08-19 10:48:44 +0200

    pending queue

--- tuxemon/technique/effects/fly_off.py
+++ tuxemon/technique/effects/fly_off.py
@@ -34,28 +34,31 @@ class FlyOffEffect(TechEffect):
     def apply(
         self, tech: Technique, user: Monster, target: Monster
     ) -> FlyOffEffectResult:
-        done = True
+        user_is_flying = False
         combat = tech.combat_state
         assert combat
 
-        # make fly the user
+        # Get the user's sprite
         user_sprite = combat._monster_sprite_map.get(user, None)
         if user_sprite and user_sprite.visible:
+            # Make the user fly
             user_sprite.visible = False
             user.out_of_range = True
-            technique = Technique()
-            technique.load(self.attack)
-            land = EnqueuedAction(user, technique, target)
-            combat._pending_queue.append(land)
+            # Create a new technique to land the user
+            land_technique = Technique()
+            land_technique.load(self.attack)
+            # Add the land action to the pending queue
+            land_action = EnqueuedAction(user, land_technique, target)
+            combat._pending_queue.append(land_action)
         else:
-            # if it's already flying
-            done = False
+            # If the user is already flying, don't do anything
+            user_is_flying = True
 
         params = {"name": user.name.upper()}
         extra = T.format("combat_fly", params)
 
         return {
-            "success": done,
+            "success": not user_is_flying,
             "damage": 0,
             "element_multiplier": 0.0,
             "should_tackle": False,

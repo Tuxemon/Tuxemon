dde95ff265c3fcff8f47053918af7845137c1187
Jaskrendix <tur3a.giorgio.menzi@gmail.com>
2025-02-11 10:20:09 +0100

    changes

--- tuxemon/db.py
+++ tuxemon/db.py
@@ -611,9 +611,10 @@ class MonsterModel(BaseModel, validate_assignment=True):
         Optional[MonsterSpritesModel], Field(validate_default=True)
     ] = None
     shape: MonsterShape = Field(..., description="The shape of the monster")
-    tags: Sequence[str] = Field(
-        ..., description="The tags of the monster", min_length=1
+    terrains: Sequence[str] = Field(
+        ..., description="The terrains of the monster"
     )
+    tags: Sequence[str] = Field(..., description="The tags of the monster")
     types: Sequence[ElementType] = Field(
         [], description="The type(s) of this monster"
     )

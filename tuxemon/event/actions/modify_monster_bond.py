b519f57ba112db10df2ad47762e0e85ff972f906
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2025-07-16 18:32:02 +0200

    variables

--- tuxemon/event/actions/modify_monster_bond.py
+++ tuxemon/event/actions/modify_monster_bond.py
@@ -4,9 +4,9 @@ from __future__ import annotations
 
 import logging
 import random as rd
-import uuid
 from dataclasses import dataclass
 from typing import Optional, Union, final
+from uuid import UUID
 
 from tuxemon.event import get_monster_by_iid
 from tuxemon.event.eventaction import EventAction
@@ -67,7 +67,7 @@ class ModifyMonsterBondAction(EventAction):
             if self.variable not in player.game_variables:
                 logger.error(f"Game variable {self.variable} not found")
                 return
-            monster_id = uuid.UUID(player.game_variables[self.variable])
+            monster_id = UUID(player.game_variables[self.variable])
             monster = get_monster_by_iid(session, monster_id)
             if monster is None:
                 logger.error("Monster not found")

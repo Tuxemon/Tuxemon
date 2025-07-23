ccf9225c041818f478b85b35701ea2452e4f71b1
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2025-07-23 14:37:11 +0200

    recompile

--- tuxemon/prepare.py
+++ tuxemon/prepare.py
@@ -311,7 +311,7 @@ def pygame_init() -> None:
     # Configure databases and locale
     from tuxemon.locale import T
 
-    T.initialize_translations()
+    T.initialize_translations(recompile=CONFIG.recompile_translations)
     from tuxemon.db import db
 
     db.load()
@@ -357,7 +357,7 @@ def headless_init() -> None:
     """Initializes game components for a headless environment."""
     from tuxemon.locale import T
 
-    T.initialize_translations()
+    T.initialize_translations(recompile=CONFIG.recompile_translations)
     from tuxemon.db import db
 
     db.load()

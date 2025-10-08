e4b8da8a557ea37f4c71afee5069ee3edc529ffa
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2025-10-08 13:07:26 +0200

    playtime

--- tuxemon/states/character.py
+++ tuxemon/states/character.py
@@ -17,8 +17,7 @@ from tuxemon.menu.menu import PygameMenuState
 from tuxemon.npc import NPC
 from tuxemon.platform.const import buttons
 from tuxemon.platform.events import PlayerInput
-from tuxemon.time_handler import today_ordinal
-from tuxemon.tools import fix_measure
+from tuxemon.tools import fix_measure, format_playtime
 
 MenuGameObj = Callable[[], object]
 lookup_cache: dict[str, MonsterModel] = {}
@@ -78,14 +77,8 @@ class CharacterState(PygameMenuState):
         msg_seen = T.format("tuxepedia_data_seen", _msg_seen)
         msg_caught = T.format("tuxepedia_data_caught", _msg_caught)
 
-        today = today_ordinal()
-        date = self.char.game_variables.get("date_start_game", today)
-        date_begin = today - int(date)
-        msg_begin = (
-            T.format("player_start_adventure", {"date": date_begin})
-            if date_begin >= 1
-            else T.translate("player_start_adventure_today")
-        )
+        play_time = format_playtime(self.char.session._total_playtime)
+        msg_begin = f"{T.translate('player_total_playtime')}: {play_time}"
 
         if self.char.battle_handler.get_battles():
             summary = self.char.battle_handler.get_battle_outcome_summary()
@@ -156,7 +149,7 @@ class CharacterState(PygameMenuState):
             float=True,
         )
         lab4.translate(fxw(0.45), fxh(0.35))
-        # begin adventure
+        # total playtime
         lab5: Any = menu.add.label(
             title=msg_begin,
             label_id="begin",

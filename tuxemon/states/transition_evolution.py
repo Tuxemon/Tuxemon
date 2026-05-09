6bdde28a9003952f8a6fe69e83915cd4f81eea1b
Sanglorian <sanglorian@gmail.com>
2026-05-09 16:51:33 +1000

    More XP from tea, fixing evolution animation
    When evolving, the original mon was a white silhouette but the evolved form was coloured. This fixes that.
    Have also increased XP grants from tea as smaller values didn't make the item worthwhile.

--- tuxemon/states/transition_evolution.py
+++ tuxemon/states/transition_evolution.py
@@ -138,6 +138,7 @@ class EvolutionTransition(State):
 
         self.evolved_sprite.image.blit(self.evolved_sprite_copy, (0, 0))
         self.evolved_sprite.image.blit(self.evolved_sprite_white, (0, 0))
+        self.evolved_sprite.image.set_alpha(255)   
 
         if self.elapsed_time > self.total_seconds and not self.dialog_opened:
             self.client.sound_manager.play("sound_confirm")
@@ -149,9 +150,10 @@ class EvolutionTransition(State):
         sprite_image = sprite.image
 
         if sprite_image is None or sprite_image.get_alpha() == 0:
-            sprite_image = (
-                self.evolved_sprite_copy
-            )  # fallback to visible image
+            if self.phase == 3:
+                sprite_image = self.evolved_sprite.image   
+            else:
+                sprite_image = self.evolved_sprite_copy    
 
         surface.blit(sprite_image, (self.x, self.y))
 
